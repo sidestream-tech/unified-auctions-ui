c426cb0842cbbe95782299ff433407b60c9ee2dc
Felix Egbert <47861849+fegbert@users.noreply.github.com>
2021-12-06 15:20:27 +0100

    feat: Roadmap and Discourse links in Footer (#292)

--- frontend/components/layout/Footer.vue
+++ frontend/components/layout/Footer.vue
@@ -21,10 +21,16 @@
                     >Report a Problem</a
                 >
             </li>
+            <li>
+                <a class="FooterLink" href="https://github.com/makerdao-sas/Roadmap" target="_blank"> Roadmap </a>
+            </li>
             <li class="flex items-center space-x-4">
                 <span class="FooterLink">Keep in touch:</span>
                 <a href="https://github.com/sidestream-tech" target="_blank"><icon type="github" class="text-xl" /></a>
                 <a href="https://twitter.com/MakerDAO_SAS" target="_blank"><icon type="twitter" class="text-xl" /></a>
+                <a href="https://forum.makerdao.com/c/core-units/sidestream-core-unit/58" target="_blank">
+                    <DiscourseIcon class="w-5 h-5 mt-1" />
+                </a>
             </li>
         </ul>
     </footer>
@@ -33,10 +39,11 @@
 <script lang="ts">
 import Vue from 'vue';
 import { Icon } from 'ant-design-vue';
+import DiscourseIcon from '~/assets/icons/discourse.svg';
 
 export default Vue.extend({
     name: 'Footer',
-    components: { Icon },
+    components: { Icon, DiscourseIcon },
     computed: {
         contactEmail(): string | undefined {
             return process.env.CONTACT_EMAIL;
