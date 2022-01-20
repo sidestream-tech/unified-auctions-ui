4fa9784689a8b0e401cdd761220afef19693925b
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-01-20 10:55:16 +0100

    Added animated Number component and story

---
+++ frontend/components/utils/AnimatedNumber.vue
@@ -0,0 +1,38 @@
+<template>
+    <animated-number
+        v-if="value"
+        :value="value.toFixed(decimalPlaces)"
+        :format-value="formatValue"
+        :duration="duration"
+    />
+</template>
+<script lang="ts">
+import AnimatedNumber from 'animated-number-vue';
+import Vue from 'vue';
+import BigNumber from 'bignumber.js';
+
+export default {
+    components: {
+        AnimatedNumber,
+    },
+    props: {
+        value: {
+            type: [Number, Object] as Vue.PropType<Number | BigNumber>,
+            default: undefined,
+        },
+        duration: {
+            type: Number,
+            default: 1000,
+        },
+        decimalPlaces: {
+            type: Number,
+            default: 2,
+        },
+    },
+    methods: {
+        formatValue(value) {
+            return value.toFixed(this.decimalPlaces);
+        },
+    },
+};
+</script>
