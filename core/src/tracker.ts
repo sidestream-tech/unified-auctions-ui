12f6ae29f145d304b350caed3b6612f7fb54f3b2
Valia Fetisov <contact@valiafetisov.com>
2022-01-26 10:13:44 +0100

    set default notifier

--- core/src/tracker.ts
+++ core/src/tracker.ts
@@ -8,59 +8,51 @@ const NUMBER_OF_BLOCKS_TO_CONFIRM = 5;
 
 const trackTransaction = async function (
     transactionPromise: Promise<ethers.ContractTransaction>,
-    notifier?: Notifier,
+    notifier: Notifier = console.info,
     confirmTransaction = true
 ): Promise<string> {
     const messageId = uuidv4();
 
     // Display initial message
-    if (notifier) {
-        notifier('loading', {
-            content: 'Transaction is preparing to be mined...',
-            key: messageId,
-            duration: 0,
-        });
-    }
+    notifier('loading', {
+        content: 'Transaction is preparing to be mined...',
+        key: messageId,
+        duration: 0,
+    });
 
     // Wait for the user approval
     let transaction: ethers.ContractTransaction;
     try {
         transaction = await transactionPromise;
-        if (notifier) {
-            notifier('loading', {
-                content: 'Transaction is pending to be mined...',
-                key: messageId,
-                duration: 0,
-            });
-        }
+        notifier('loading', {
+            content: 'Transaction is pending to be mined...',
+            key: messageId,
+            duration: 0,
+        });
     } catch (error: any) {
-        if (notifier) {
-            notifier('error', {
-                content: `Transaction error: ${truncateText(error?.message || 'unknown')}`,
-                key: messageId,
-                duration: DEFAULT_NOTIFICATION_DURATION,
-            });
-        }
+        notifier('error', {
+            content: `Transaction error: ${truncateText(error?.message || 'unknown')}`,
+            key: messageId,
+            duration: DEFAULT_NOTIFICATION_DURATION,
+        });
         throw new Error(error);
     }
 
     try {
         // Wait for mining
         const minedTransactionReceipt = await transaction.wait(1);
-        if (notifier) {
-            if (confirmTransaction) {
-                notifier('loading', {
-                    content: `Transaction was mined into block "${minedTransactionReceipt.blockNumber}", confirming...`,
-                    key: messageId,
-                    duration: 0,
-                });
-            } else {
-                notifier('success', {
-                    content: `Transaction was mined into block "${minedTransactionReceipt.blockNumber}"`,
-                    key: messageId,
-                    duration: DEFAULT_NOTIFICATION_DURATION,
-                });
-            }
+        if (confirmTransaction) {
+            notifier('loading', {
+                content: `Transaction was mined into block "${minedTransactionReceipt.blockNumber}", confirming...`,
+                key: messageId,
+                duration: 0,
+            });
+        } else {
+            notifier('success', {
+                content: `Transaction was mined into block "${minedTransactionReceipt.blockNumber}"`,
+                key: messageId,
+                duration: DEFAULT_NOTIFICATION_DURATION,
+            });
         }
         if (!confirmTransaction) {
             return minedTransactionReceipt.transactionHash;
@@ -68,22 +60,18 @@ const trackTransaction = async function (
 
         // Wait for confirmation
         const confirmedTransactionReceipt = await transaction.wait(NUMBER_OF_BLOCKS_TO_CONFIRM);
-        if (notifier) {
-            notifier('success', {
-                content: `Transaction was confirmed in block "${confirmedTransactionReceipt.blockNumber}"`,
-                key: messageId,
-                duration: DEFAULT_NOTIFICATION_DURATION,
-            });
-        }
+        notifier('success', {
+            content: `Transaction was confirmed in block "${confirmedTransactionReceipt.blockNumber}"`,
+            key: messageId,
+            duration: DEFAULT_NOTIFICATION_DURATION,
+        });
         return confirmedTransactionReceipt.transactionHash;
     } catch (error: any) {
-        if (notifier) {
-            notifier('error', {
-                content: `Transaction was rejected with error: "${truncateText(error?.message || 'unknown')}"`,
-                key: messageId,
-                duration: DEFAULT_NOTIFICATION_DURATION,
-            });
-        }
+        notifier('error', {
+            content: `Transaction was rejected with error: "${truncateText(error?.message || 'unknown')}"`,
+            key: messageId,
+            duration: DEFAULT_NOTIFICATION_DURATION,
+        });
         throw new Error(error);
     }
 };
