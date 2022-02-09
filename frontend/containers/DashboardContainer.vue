75d116efd34e58bea1e695585cdabe28c06a9e4f
Felix Egbert <47861849+fegbert@users.noreply.github.com>
2022-02-09 09:28:35 +0100

    feat: Callee addresses in the dashboard (#78)

--- frontend/containers/DashboardContainer.vue
+++ frontend/containers/DashboardContainer.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="DashboardContainer">
-        <DashboardAuctionsView :collaterals="collaterals" :is-explanations-shown.sync="isExplanationsShown" />
+        <DashboardAuctionsView
+            :collaterals="collaterals"
+            :callees="callees"
+            :is-explanations-shown.sync="isExplanationsShown"
+        />
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import { mapGetters } from 'vuex';
+import { getCalleesByNetworkType } from 'auctions-core/src/constants/CALLEES';
 import DashboardAuctionsView from '~/components/DashboardAuctionsView.vue';
 
 export default Vue.extend({
@@ -24,6 +29,13 @@ export default Vue.extend({
             set(newIsExplanationsShown): void {
                 this.$store.dispatch('preferences/setExplanationsAction', newIsExplanationsShown);
             },
+        },
+        callees(): CalleeAddresses {
+            const network = this.$store?.getters['network/getMakerNetwork'];
+            if (!network) {
+                return {};
+            }
+            return getCalleesByNetworkType(network);
         },
     },
     async mounted() {
