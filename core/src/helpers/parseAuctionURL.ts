5c72b4413bec63381b16a041fe0671ac480db877
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-09 10:39:49 +0100

    Added checks to auctionURL

--- core/src/helpers/parseAuctionURL.ts
+++ core/src/helpers/parseAuctionURL.ts
@@ -1,11 +1,21 @@
+import { getCollateralConfigByType } from '../constants/COLLATERALS';
+
 function parseAuctionURL(url: string): { collateralType: string; auctionId: number } {
     const parts = url.split(':', 1);
     const collateralType = parts[0];
     const auctionId = parts[1];
 
+    // Check that AuctionId is a number
+    if (!/\D/.test(auctionId)) {
+        throw new Error(`"${auctionId}" is not a valid Auction ID.`);
+    }
+
+    // Check if we support the CollateralType
+    getCollateralConfigByType(collateralType);
+
     return {
         collateralType,
-        auctionId,
+        auctionId: parseInt(auctionId),
     };
 }
 
