dd6882ad6cb84ee54d95b8e5bd933a31ec61a702
valia fetisov <contact@valiafetisov.com>
2022-02-10 16:17:04 +0100

    feat: WstETH collateral support (#92)

--- core/src/types.ts
+++ core/src/types.ts
@@ -58,7 +58,7 @@ export declare interface AuctionTransaction extends Auction, TransactionFees {
 }
 
 export declare interface UniswapV2CalleeConfig {
-    callee: 'UniswapV2CalleeDai';
+    callee: 'UniswapV2CalleeDai' | 'WstETHCurveUniv3Callee';
     route: string[];
 }
 
@@ -88,6 +88,7 @@ export declare interface NetworkConfig {
 export declare interface CalleeAddresses {
     UniswapV2CalleeDai: string;
     UniswapV2LpTokenCalleeDai: string;
+    WstETHCurveUniv3Callee?: string;
 }
 
 export type CalleeNames = keyof CalleeAddresses;
