bcf91fbf71749ef06ca19896193fcba519f1fb47
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-13 23:52:16 +0100

    memoizeed authorize wallet and authorize collateral

--- core/src/authorizations.ts
+++ core/src/authorizations.ts
@@ -1,20 +1,23 @@
 import type { Notifier } from './types';
 import getContract, { getContractAddressByName, getClipperNameByCollateralType } from './contracts';
 import executeTransaction from './execute';
+import memoizee from 'memoizee';
 
-// Make this memoizzed
-export const authorizeWallet = async function (
-    network: string,
-    revoke: boolean,
-    notifier?: Notifier
-): Promise<string> {
+const AUTHORIZE_WALLET_CACHE_MS = 30 * 1000;
+
+const _authorizeWallet = async function (network: string, revoke: boolean, notifier?: Notifier): Promise<string> {
     const joinDaiAddress = await getContractAddressByName(network, 'MCD_JOIN_DAI');
     const contractMethod = revoke ? 'nope' : 'hope';
     return executeTransaction(network, 'MCD_VAT', contractMethod, [joinDaiAddress], notifier);
 };
 
-// Make this memoizzed
-export const authorizeCollateral = async function (
+export const authorizeWallet = memoizee(_authorizeWallet, {
+    maxAge: AUTHORIZE_WALLET_CACHE_MS,
+    promise: true,
+    length: 3,
+});
+
+const _authorizeCollateral = async function (
     network: string,
     collateralType: string,
     revoke: boolean,
@@ -25,6 +28,12 @@ export const authorizeCollateral = async function (
     const contractMethod = revoke ? 'nope' : 'hope';
     return executeTransaction(network, 'MCD_VAT', contractMethod, [clipperAddress], notifier);
 };
+
+export const authorizeCollateral = memoizee(_authorizeCollateral, {
+    maxAge: AUTHORIZE_WALLET_CACHE_MS,
+    promise: true,
+    length: 4,
+});
 
 export const getWalletAuthorizationStatus = async function (network: string, walletAddress: string): Promise<boolean> {
     const joinDaiAddress = await getContractAddressByName(network, 'MCD_JOIN_DAI');
