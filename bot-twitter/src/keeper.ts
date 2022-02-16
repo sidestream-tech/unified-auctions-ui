419106f158ccba7ef4da8b057e611fdeda4dbea8
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-16 17:04:20 +0100

    Added MIN_PROFIT_DAI

--- bot-twitter/src/keeper.ts
+++ bot-twitter/src/keeper.ts
@@ -1,13 +1,16 @@
 import { AuctionInitialInfo } from 'auctions-core/src/types';
 import getSigner from 'auctions-core/src/signer';
 import { enrichAuction } from 'auctions-core/src/auctions';
+
 const ETHEREUM_NETWORK = process.env.ETHEREUM_NETWORK || 'kovan';
+const MIN_PROFIT_DAI = process.env.MIN_PROFIT_DAI || 100;
 
 async function participate(auction: AuctionInitialInfo) {
     const signer = getSigner(ETHEREUM_NETWORK);
     console.info(signer);
     const auctionTransaction = await enrichAuction(ETHEREUM_NETWORK, auction);
     console.info(auctionTransaction);
+    console.info(MIN_PROFIT_DAI);
     /*
       NOTE: Participate has to be able to be called multiple times even if the last execution might still be running without breaking anything!
 
@@ -15,10 +18,6 @@ async function participate(auction: AuctionInitialInfo) {
       enrich auction from AuctionInitalInfo to AuctionTransaction
       check if profitMinusFees is bigger than AUCTION_MIN_PROFIT (env variable)
       if yes process to the first authorisation, if no return
-
-      cache authorisation steps (memozizze)
-      if (!auth1) authorize auth1; (authorizeWallet)
-      recall the function with fresh auction
 
       if (!auth2) authorize auth2; (authorizeCollateral)
       recall the function with fresh auction
