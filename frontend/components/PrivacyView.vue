c0baeab9c286a689dc10827d00229c31ef220b4a
Felix Egbert <fekiegbert@googlemail.com>
2022-02-22 11:48:01 +0100

    added Privacy Page structure

---
+++ frontend/components/PrivacyView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+    components: {},
+});
+</script>
+
+<style scoped>
+.LandingBlock {
+    min-height: 33vh;
+}
+</style>
