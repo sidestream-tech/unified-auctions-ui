0dca0899cca23c52e35a866f1298e878731ee91a
Felix Egbert <fekiegbert@googlemail.com>
2022-02-23 13:07:00 +0100

    ignoring unused vars warning since we only want to filter it out

--- core/src/constants/NETWORKS.ts
+++ core/src/constants/NETWORKS.ts
@@ -85,6 +85,6 @@ export const getNetworks = function (isDev: boolean): Record<string, NetworkConf
     if (isDev) {
         return NETWORKS;
     }
-    const { localhost, ...otherNetworks } = NETWORKS;
+    const { localhost, ...otherNetworks } = NETWORKS; //eslint-disable-line
     return otherNetworks;
 };
