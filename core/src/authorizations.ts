9094927b445d6294d044fce82b53ee70dce99982
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-02-23 17:08:18 +0100

    Changed caching reset location in code

--- core/src/authorizations.ts
+++ core/src/authorizations.ts
@@ -12,8 +12,9 @@ export const authorizeWallet = async function (
     const signer = await getSigner(network);
     const joinDaiAddress = await getContractAddressByName(network, 'MCD_JOIN_DAI');
     const contractMethod = revoke ? 'nope' : 'hope';
+    const transaction = executeTransaction(network, 'MCD_VAT', contractMethod, [joinDaiAddress], notifier);
     await getWalletAuthorizationStatus.delete(network, await signer.getAddress());
-    return executeTransaction(network, 'MCD_VAT', contractMethod, [joinDaiAddress], notifier);
+    return transaction;
 };
 
 export const authorizeCollateral = async function (
@@ -26,8 +27,9 @@ export const authorizeCollateral = async function (
     const contractName = getClipperNameByCollateralType(collateralType);
     const clipperAddress = await getContractAddressByName(network, contractName);
     const contractMethod = revoke ? 'nope' : 'hope';
+    const transaction = executeTransaction(network, 'MCD_VAT', contractMethod, [clipperAddress], notifier);
     await getCollateralAuthorizationStatus.delete(network, collateralType, await signer.getAddress());
-    return executeTransaction(network, 'MCD_VAT', contractMethod, [clipperAddress], notifier);
+    return transaction;
 };
 
 const _getWalletAuthorizationStatus = async function (network: string, walletAddress: string): Promise<boolean> {
