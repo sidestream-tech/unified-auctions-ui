3d17099bdc12d734a184bc1557552f3524fe7f7a
Felix Egbert <fekiegbert@googlemail.com>
2022-03-01 11:57:50 +0100

    added TransactionMessage Component

---
+++ frontend/components/transaction/TransactionMessage.stories.js
@@ -0,0 +1,22 @@
+import { storiesOf } from '@storybook/vue';
+import faker from 'faker';
+import TransactionMessage from './TransactionMessage';
+
+const common = {
+    components: { TransactionMessage },
+};
+
+storiesOf('Transaction/TransactionMessage', module)
+    .add('Default', () => ({
+        ...common,
+        template: '<TransactionMessage />',
+    }))
+    .add('Finished', () => ({
+        ...common,
+        data() {
+            return {
+                transactionAddress: faker.finance.ethereumAddress(),
+            };
+        },
+        template: '<TransactionMessage is-finished :transaction-address="transactionAddress" />',
+    }));
