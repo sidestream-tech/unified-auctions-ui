6d2e2ab6ecdd71c1a6b1ff8485ea32b25e0ee39f
Felix Egbert <fekiegbert@googlemail.com>
2022-03-01 15:03:20 +0100

    added stories

--- frontend/components/transaction/DepositBlock.stories.js
+++ frontend/components/transaction/DepositBlock.stories.js
@@ -16,13 +16,64 @@ const common = {
     },
 };
 
-storiesOf('Transaction/DepositBlock', module).add('Default', () => ({
-    ...common,
-    template: `
+storiesOf('Transaction/DepositBlock', module)
+    .add('Default', () => ({
+        ...common,
+        template: `
     <DepositBlock 
         :walletDAI="walletDAI"
         :vatDAI="vatDAI"
         :transactionAmountDAI="transactionAmountDAI"
-    />
-    `,
-}));
+    />`,
+    }))
+    .add('Disabled', () => ({
+        ...common,
+        template: `
+    <DepositBlock 
+        :walletDAI="walletDAI"
+        :vatDAI="vatDAI"
+        :transactionAmountDAI="transactionAmountDAI"
+        disabled
+    />`,
+    }))
+    .add('Disabled and missing walletDAI and vatDAI', () => ({
+        ...common,
+        template: `
+        <DepositBlock
+            :transactionAmountDAI="transactionAmountDAI"
+            disabled
+        />`,
+    }))
+    .add('Loading', () => ({
+        ...common,
+        template: `
+        <DepositBlock 
+            :walletDAI="walletDAI"
+            :vatDAI="vatDAI"
+            :transactionAmountDAI="transactionAmountDAI"
+            is-loading
+        />`,
+    }))
+    .add('Missing walletDAI and vatDAI', () => ({
+        ...common,
+        template: `
+        <DepositBlock 
+            :transactionAmountDAI="transactionAmountDAI"
+        />`,
+    }))
+    .add('Minimum to deposit is 0', () => ({
+        components: { DepositBlock },
+        data() {
+            return {
+                walletDAI: new BigNumber(faker.finance.amount()),
+                vatDAI: new BigNumber(100),
+                transactionAmountDAI: new BigNumber(100),
+            };
+        },
+        template: `
+        <DepositBlock 
+            :walletDAI="walletDAI"
+            :vatDAI="vatDAI"
+            :transactionAmountDAI="transactionAmountDAI"
+        />`,
+    }));
