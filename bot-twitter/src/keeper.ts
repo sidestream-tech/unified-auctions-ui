aee68c7527fa0b2d3feac53369a1931b0624c3d3
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-03-02 15:27:53 +0100

    Added try catch for keeper

--- bot-twitter/src/keeper.ts
+++ bot-twitter/src/keeper.ts
@@ -32,69 +32,70 @@ async function participate(auction: AuctionInitialInfo) {
     if (!areKeeperVariablesSet()) {
         return;
     }
-    const signer = await getSigner(ETHEREUM_NETWORK);
+    try {
+        const signer = await getSigner(ETHEREUM_NETWORK);
+        // enrich the auction with more numbers
+        const auctionTransaction = await enrichAuction(ETHEREUM_NETWORK, auction);
 
-    // enrich the auction with more numbers
-    const auctionTransaction = await enrichAuction(ETHEREUM_NETWORK, auction);
+        if (auctionTransaction.isFinished || !auctionTransaction.isActive) {
+            console.info(`Auction "${auction.id}" is not active or has already finished.`);
+            return;
+        }
 
-    if (auctionTransaction.isFinished || !auctionTransaction.isActive) {
-        console.info(`Auction "${auction.id}" is not active or has already finished.`);
-        return;
-    }
+        // check if the profit of the auction is worth executing
+        if (
+            auctionTransaction.transactionProfitMinusFees &&
+            auctionTransaction.transactionProfitMinusFees.toNumber() < KEEPER_MIN_PROFIT_DAI
+        ) {
+            console.info(
+                `Auction "${
+                    auction.id
+                }" clear profit is smaller than min profit (${auctionTransaction.transactionProfitMinusFees.toFixed()} < ${KEEPER_MIN_PROFIT_DAI})`
+            );
+            return;
+        }
 
-    // check if the profit of the auction is worth executing
-    if (
-        auctionTransaction.transactionProfitMinusFees &&
-        auctionTransaction.transactionProfitMinusFees.toNumber() < KEEPER_MIN_PROFIT_DAI
-    ) {
-        console.info(
-            `Auction "${
-                auction.id
-            }" clear profit is smaller than min profit (${auctionTransaction.transactionProfitMinusFees.toFixed()} < ${KEEPER_MIN_PROFIT_DAI})`
-        );
-        return;
-    }
+        // get wallet authorization status
+        const walletAddress = await signer.getAddress();
+        const isAuth = await getWalletAuthorizationStatus(ETHEREUM_NETWORK, walletAddress);
 
-    // get wallet authorization status
-    const walletAddress = await signer.getAddress();
-    const isAuth = await getWalletAuthorizationStatus(ETHEREUM_NETWORK, walletAddress);
+        // try to authorize the wallet then return
+        if (!isAuth) {
+            console.info(`Wallet "${walletAddress}" has not been authorized yet. Attempting authorization now`);
+            const transactionHash = await authorizeWallet(ETHEREUM_NETWORK, false);
+            console.info(`Wallet "${walletAddress}" successfully authorized via "${transactionHash}" transaction`);
+            await participate(auction);
+            return;
+        }
 
-    // try to authorize the wallet then return
-    if (!isAuth) {
-        console.info(`Wallet "${walletAddress}" has not been authorized yet. Attempting authorization now`);
-        const transactionHash = await authorizeWallet(ETHEREUM_NETWORK, false);
-        console.info(`Wallet "${walletAddress}" successfully authorized via "${transactionHash}" transaction`);
-        await participate(auction);
-        return;
-    }
-
-    // get collateral authorization status
-    const isCollateralAuth = await getCollateralAuthorizationStatus(
-        ETHEREUM_NETWORK,
-        auctionTransaction.collateralType,
-        walletAddress
-    );
-
-    // try to authorize the collateral then return
-    if (!isCollateralAuth) {
-        console.info(
-            `Collateral "${auctionTransaction.collateralType}" has not been authorized on wallet "${walletAddress}" yet. Attempting authorization now`
-        );
-        const collateralTransactionHash = await authorizeCollateral(
+        // get collateral authorization status
+        const isCollateralAuth = await getCollateralAuthorizationStatus(
             ETHEREUM_NETWORK,
             auctionTransaction.collateralType,
-            false
+            walletAddress
         );
-        console.info(
-            `Collateral "${auctionTransaction.collateralType}" successfully authorized on wallet "${walletAddress}" via "${collateralTransactionHash}" transaction`
-        );
-        await participate(auction);
-        return;
-    }
 
-    // Bid on the Auction
-    const bidHash = await bidOnTheAuction(ETHEREUM_NETWORK, auctionTransaction, walletAddress);
-    console.info(`Auction "${auctionTransaction.id}" was executed via "${bidHash}" transaction`);
+        // try to authorize the collateral then return
+        if (!isCollateralAuth) {
+            console.info(
+                `Collateral "${auctionTransaction.collateralType}" has not been authorized on wallet "${walletAddress}" yet. Attempting authorization now`
+            );
+            const collateralTransactionHash = await authorizeCollateral(
+                ETHEREUM_NETWORK,
+                auctionTransaction.collateralType,
+                false
+            );
+            console.info(
+                `Collateral "${auctionTransaction.collateralType}" successfully authorized on wallet "${walletAddress}" via "${collateralTransactionHash}" transaction`
+            );
+            await participate(auction);
+            return;
+        }
+
+        // Bid on the Auction
+        const bidHash = await bidOnTheAuction(ETHEREUM_NETWORK, auctionTransaction, walletAddress);
+        console.info(`Auction "${auctionTransaction.id}" was executed via "${bidHash}" transaction`);
+    } catch {}
 }
 
 export default participate;
