6967562d81bbf596fc0c6700dbd5eef8bfd5bf03
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-03-10 15:36:57 +0100

    Added WalletTable

---
+++ frontend/components/wallet/WalletTable.stories.js
@@ -0,0 +1,31 @@
+import { storiesOf } from '@storybook/vue';
+import faker from 'faker';
+import BigNumber from 'bignumber.js';
+import { action } from '@storybook/addon-actions';
+import WalletTable from '~/components/wallet/WalletTable';
+
+const common = {
+    components: { WalletTable },
+    data: () => ({
+        walletAddress: faker.finance.ethereumAddress(),
+        walletETH: new BigNumber(faker.finance.amount()),
+        walletDAI: new BigNumber(faker.finance.amount()),
+        walletVatDAI: new BigNumber(faker.finance.amount()),
+        walletLastUpdatedDate: faker.date.recent(),
+    }),
+    methods: {
+        refresh: action('refresh'),
+    },
+};
+
+storiesOf('Wallet/WalletTable', module)
+    .add('Default', () => ({
+        ...common,
+        template:
+            '<WalletTable :walletAddress="walletAddress" :walletETH="walletETH" :walletDAI="walletDAI" :walletVatDAI="walletVatDAI" :walletLastUpdatedDate="walletLastUpdatedDate" @refresh="refresh" />',
+    }))
+    .add('Loading', () => ({
+        ...common,
+        template:
+            '<WalletTable :is-loading="true" :walletAddress="walletAddress" :walletETH="walletETH" :walletDAI="walletDAI" :walletVatDAI="walletVatDAI" :walletLastUpdatedDate="walletLastUpdatedDate" @refresh="refresh" />',
+    }));
---
+++ frontend/components/wallet/WalletTable.vue
@@ -0,0 +1,92 @@
+<template>
+    <Loading :is-loading="isLoading">
+        <table class="w-full table-fixed border-collapse border">
+            <tbody>
+                <tr>
+                    <td>Wallet Address</td>
+                    <td>
+                        <FormatAddress :value="walletAddress" :shorten="true" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>ETH amount in wallet</td>
+                    <td>
+                        <FormatCurrency :value="walletETH" currency="ETH" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>DAI amount in wallet</td>
+                    <td>
+                        <FormatCurrency :value="walletDAI" currency="DAI" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Currently deposited in VAT</td>
+                    <td>
+                        <FormatCurrency :value="walletVatDAI" currency="DAI" />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Last updated</td>
+                    <td>
+                        <TimeTill :date="walletLastUpdatedDate" />
+                        <button class="text-green-600 hover:text-green-400 underline ml-1" @click="$emit('refresh')">
+                            Refresh
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </Loading>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BigNumber from 'bignumber.js';
+import FormatAddress from '../utils/FormatAddress.vue';
+import FormatCurrency from '../utils/FormatCurrency.vue';
+import TimeTill from '../common/TimeTill.vue';
+import Loading from '../common/Loading.vue';
+
+export default Vue.extend({
+    name: 'WalletTable',
+    components: { Loading, TimeTill, FormatCurrency, FormatAddress },
+    props: {
+        walletAddress: {
+            type: String,
+            required: true,
+        },
+        walletETH: {
+            type: Object as Vue.PropType<BigNumber>,
+            required: true,
+        },
+        walletDAI: {
+            type: Object as Vue.PropType<BigNumber>,
+            required: true,
+        },
+        walletVatDAI: {
+            type: Object as Vue.PropType<BigNumber>,
+            required: true,
+        },
+        walletLastUpdatedDate: {
+            type: [String, Number, Date],
+            required: true,
+        },
+        isLoading: {
+            type: Boolean,
+            default: false,
+        },
+    },
+});
+</script>
+
+<style scoped>
+th,
+td {
+    @apply p-2 border-2 border-collapse border-gray-300 dark:border-gray-600 dark:text-gray-200;
+}
+
+tr td:nth-child(2) {
+    @apply text-right;
+}
+</style>
