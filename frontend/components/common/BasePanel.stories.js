d710ad5bfb8fbbb79d5506ff87eff5faea3f0fd5
simontree <simonroof@tutanota.com>
2022-03-23 15:42:28 +0100

    added 5 states with styles included

---
+++ frontend/components/common/BasePanel.stories.js
@@ -0,0 +1,39 @@
+// import { action } from '@storybook/addon-actions';
+import { storiesOf } from '@storybook/vue';
+import faker from 'faker';
+import BasePanel from '~/components/common/BasePanel';
+
+const common = {
+    components: { BasePanel },
+    data: () => ({
+        inactive: faker.lorem.sentence(4),
+        incorrect: faker.lorem.sentence(20),
+        complete: faker.lorem.sentence(20),
+        notice: faker.lorem.sentence(20),
+        walletAdress: faker.finance.bitcoinAddress(),
+    }),
+    methods: {},
+};
+
+storiesOf('Common/BasePanel', module)
+    .add('Default', () => ({
+        ...common,
+        template: '<BasePanel v-bind="$data" state="default"> This is a Default BasePanel </BasePanel>',
+    }))
+    .add('Inactive', () => ({
+        ...common,
+        template: '<BasePanel v-bind="$data" state="inactive"> (ICON) No wallet connected </BasePanel>',
+    }))
+    .add('Incorrect', () => ({
+        ...common,
+        template: '<BasePanel v-bind="$data" state="incorrect"> The amount is not present in the VAT </BasePanel>',
+    }))
+    .add('Complete', () => ({
+        ...common,
+        template: '<BasePanel v-bind="$data" state="complete"> (ICON) The amount is present in the VAT </BasePanel>',
+    }))
+    .add('Notice', () => ({
+        ...common,
+        template:
+            '<BasePanel v-bind="$data" state="notice"> (ICON) The transaction net profit is negative for wallet with address: {{ walletAdress }}</BasePanel>',
+    }));
---
+++ frontend/components/common/BasePanel.vue
@@ -0,0 +1,69 @@
+<template>
+    <modal :visible="true" :mask-closable="false" :closable="false" :footer="null">
+        <!-- Default Base Panel -->
+        <div v-if="state === 'default'">
+            <div class="p-5 gap-5 text-lg font-bold break-words"><slot /></div>
+        </div>
+        <!-- Inactive Base Panel -->
+        <div v-if="state === 'inactive'">
+            <div class="p-5 gap-5 text-lg font-bold text-gray-500 break-words">
+                <slot />
+            </div>
+        </div>
+        <!-- Incorrect Base Panel -->
+        <div v-if="state === 'incorrect'">
+            <div class="p-5 gap-5 text-lg font-bold text-red-500 break-words">
+                <slot />
+            </div>
+            <div class="border-solid border-2 p-5 gap-5">
+                <div class="grid grid-cols-2">
+                    <span>Amount in wallet:</span>
+                    <span>100 DAI</span>
+                </div>
+                <div>lorem ipsum</div>
+                <div class="flex justify-end p-5 gap-5">
+                    <BaseButton type="primary">Click here</BaseButton>
+                </div>
+            </div>
+        </div>
+        <!-- Complete Base Panel -->
+        <div v-if="state === 'complete'">
+            <div class="p-5 gap-5 text-lg font-bold text-gray-500 break-words">
+                <slot />
+            </div>
+            <div class="border-solid border-2 p-5 gap-5">All good</div>
+        </div>
+        <!-- Notice Base Panel -->
+        <div v-if="state === 'notice'">
+            <div class="p-5 gap-5 text-lg font-bold text-yellow-500 break-words">
+                <slot />
+            </div>
+            <div class="border-solid border-2 p-5 gap-5">Lorem ipsum...</div>
+        </div>
+    </modal>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Modal } from 'ant-design-vue';
+import BaseButton from './BaseButton.vue';
+
+export default Vue.extend({
+    name: 'BasePanel',
+    components: { Modal, BaseButton },
+    props: {
+        state: {
+            type: String,
+            default: '',
+        },
+        title: {
+            type: String,
+            default: '',
+        },
+        walletAdress: {
+            type: String,
+            required: true,
+        },
+    },
+});
+</script>
