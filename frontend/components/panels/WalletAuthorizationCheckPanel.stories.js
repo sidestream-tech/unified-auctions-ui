e0632f329e15176e0961dcb8d3af0337e922c1f3
Felix Egbert <fekiegbert@googlemail.com>
2022-04-04 13:23:00 +0200

    WalletAuthorizationCheckPanel stories

--- frontend/components/panels/WalletAuthorizationCheckPanel.stories.js
+++ frontend/components/panels/WalletAuthorizationCheckPanel.stories.js
@@ -1,11 +1,100 @@
 import { storiesOf } from '@storybook/vue';
+import faker from 'faker';
 import WalletAuthorizationCheckPanel from './WalletAuthorizationCheckPanel';
 
 const common = {
     components: { WalletAuthorizationCheckPanel },
+    data() {
+        return {
+            isWalletAuthorized: false,
+            isLoading: false,
+            walletAddress: faker.finance.ethereumAddress(),
+        };
+    },
+    methods: {
+        authorize() {
+            this.isLoading = true;
+            setTimeout(() => {
+                this.isWalletAuthorized = true;
+                this.isLoading = false;
+            }, 1000);
+        },
+    },
 };
 
-storiesOf('Panels/WalletAuthorizationCheckPanel', module).add('Default', () => ({
-    ...common,
-    template: '<WalletAuthorizationCheckPanel />',
-}));
+storiesOf('Panels/WalletAuthorizationCheckPanel', module)
+    .add('Authorized', () => ({
+        ...common,
+        data() {
+            return {
+                isWalletAuthorized: true,
+                walletAddress: faker.finance.ethereumAddress(),
+            };
+        },
+        template: `
+        <WalletAuthorizationCheckPanel
+            :is-wallet-authorized="isWalletAuthorized"
+            :wallet-address="walletAddress"
+            :is-loading="isLoading"
+            @authorizeWallet="authorize"
+        />`,
+    }))
+    .add('Unauthorized', () => ({
+        ...common,
+        template: `
+        <WalletAuthorizationCheckPanel
+            :is-wallet-authorized="isWalletAuthorized"
+            :wallet-address="walletAddress"
+            :is-loading="isLoading"
+            @authorizeWallet="authorize()"
+        />`,
+    }))
+    .add('Missing Wallet', () => ({
+        ...common,
+        data() {
+            return {
+                isWalletAuthorized: false,
+                walletAddress: undefined,
+            };
+        },
+        template: `
+        <WalletAuthorizationCheckPanel
+            :is-wallet-authorized="isWalletAuthorized"
+            :wallet-address="walletAddress"
+            :is-loading="isLoading"
+            @authorizeWallet="authorize"
+        />`,
+    }))
+    .add('Loading', () => ({
+        ...common,
+        template: `
+        <WalletAuthorizationCheckPanel
+            :is-wallet-authorized="isWalletAuthorized"
+            :wallet-address="walletAddress"
+            :is-loading="isLoading"
+            @authorizeWallet="authorize"
+            is-loading
+        />`,
+    }))
+    .add('Disabled', () => ({
+        ...common,
+        template: `
+        <WalletAuthorizationCheckPanel
+            :is-wallet-authorized="isWalletAuthorized"
+            :wallet-address="walletAddress"
+            :is-loading="isLoading"
+            @authorizeWallet="authorize"
+            disabled
+        />`,
+    }))
+    .add('Expert Mode', () => ({
+        ...common,
+        template: `
+        <WalletAuthorizationCheckPanel
+            :is-wallet-authorized="isWalletAuthorized"    
+            :wallet-address="walletAddress"
+            :is-loading="isLoading"
+            @authorizeWallet="authorize"
+            :is-explanations-shown="false"
+        />`,
+    }));
