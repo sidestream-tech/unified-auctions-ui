c89a9ec327c87a00a6425ad707a4b51c99bab49c
Felix Egbert <fekiegbert@googlemail.com>
2022-04-05 11:22:00 +0200

    Added WalletConnectionCheckPanel and stories

---
+++ frontend/components/panels/WalletConnectionCheckPanel.vue
@@ -0,0 +1,87 @@
+<template>
+    <BasePanel :current-state="currentStateAndTitle.name">
+        <template #title>
+            <span v-if="currentStateAndTitle.name === 'correct'">
+                Wallet <format-address disable shorten :value="walletAddress" /> is connected
+            </span>
+            <span v-else>{{ currentStateAndTitle.title }}</span>
+        </template>
+        <TextBlock v-if="isExplanationsShown">
+            If you do not have a wallet yet, you can simply sign up for it on a service like
+            <a href="https://metamask.io/" target="_blank">MetaMask</a>.
+        </TextBlock>
+        <div class="flex justify-end mt-2">
+            <BaseButton
+                v-if="!walletAddress"
+                type="primary"
+                :is-loading="isLoading"
+                :disabled="disabled"
+                @click="$emit('connectWallet')"
+            >
+                <span v-if="isLoading">Connecting...</span>
+                <span v-else>Connect a wallet</span>
+            </BaseButton>
+            <BaseButton v-else :is-loading="isLoading" :disabled="disabled" @click="$emit('disconnectWallet')">
+                <span v-if="isLoading">Disconnecting...</span>
+                <span v-else>Disconnect wallet <format-address disable shorten :value="walletAddress" /></span>
+            </BaseButton>
+        </div>
+    </BasePanel>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BaseButton from '~/components/common/BaseButton.vue';
+import BasePanel from '~/components/common/BasePanel.vue';
+import TextBlock from '~/components/common/TextBlock.vue';
+import FormatAddress from '~/components/utils/FormatAddress.vue';
+
+export default Vue.extend({
+    components: {
+        BaseButton,
+        BasePanel,
+        TextBlock,
+        FormatAddress,
+    },
+    props: {
+        walletAddress: {
+            type: String,
+            required: true,
+        },
+        disabled: {
+            type: Boolean,
+            default: false,
+        },
+        isLoading: {
+            type: Boolean,
+            default: false,
+        },
+        isExplanationsShown: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    computed: {
+        currentStateAndTitle(): PanelProps {
+            if (!this.walletAddress) {
+                return {
+                    name: 'incorrect',
+                    title: 'Wallet is not connected',
+                };
+            }
+            return {
+                name: 'correct',
+                title: `Wallet is connected`,
+            };
+        },
+    },
+    watch: {
+        currentStateAndTitle: {
+            immediate: true,
+            handler(newCurrentStateAndTitle) {
+                this.$emit('update:isCorrect', newCurrentStateAndTitle.name === 'correct');
+            },
+        },
+    },
+});
+</script>
