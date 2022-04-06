17f195e715b6ae906bb3f9db389244adaa82d8e9
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-04-06 12:35:07 +0200

    Added NetProfitCheckPanel

---
+++ frontend/components/panels/NetProfitCheckPanel.stories.js
@@ -0,0 +1,48 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import faker from 'faker';
+import BigNumber from 'bignumber.js';
+import NetProfitCheckPanel from "~/components/panels/NetProfitCheckPanel";
+
+const common = {
+    components: { NetProfitCheckPanel },
+    data: () => ({
+        netProfit: new BigNumber(faker.finance.amount(100, 200)),
+    }),
+    methods: {
+        isCorrect: action('isCorrect'),
+    },
+    template: `
+    <NetProfitCheckPanel 
+        v-bind="$data"
+        @update:isCorrect="isCorrect"
+    />
+    `,
+};
+
+storiesOf('Panels/NetProfitCheckPanel', module)
+    .add('Negative Net Profit', () => ({
+        ...common,
+        data: () => ({
+            ...common.data(),
+            netProfit: new BigNumber(-10),
+        }),
+    }))
+    .add('Positive Net Profit', () => ({
+        ...common,
+    }))
+    .add('Unknown Net Profit', () => ({
+        ...common,
+        data: () => ({
+            ...common.data(),
+            netProfit: undefined,
+        }),
+    }))
+    .add('Expert Mode', () => ({
+        ...common,
+        data: () => ({
+            ...common.data(),
+            isExplanationsShown: false,
+            netProfit: new BigNumber(-10),
+        }),
+    }));
---
+++ frontend/components/panels/NetProfitCheckPanel.vue
@@ -0,0 +1,59 @@
+<template>
+    <BasePanel :current-state="currentStateAndTitle.name" :disabled="!isExplanationsShown">
+        <template #title> {{ currentStateAndTitle.title }} </template>
+        <TextBlock v-if="isExplanationsShown">
+            Executing an auction with a negative net profit is possible, but according to our calculations it will
+            result in the net loss due to the transaction fees being higher then the gross profit.
+        </TextBlock>
+    </BasePanel>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BigNumber from 'bignumber.js';
+import BasePanel from '../common/BasePanel.vue';
+import TextBlock from '../common/TextBlock.vue';
+
+export default Vue.extend({
+    name: 'NetProfitCheckPanel',
+    components: { TextBlock, BasePanel },
+    props: {
+        netProfit: {
+            type: Object as Vue.PropType<BigNumber>,
+            default: undefined,
+        },
+        isExplanationsShown: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    computed: {
+        currentStateAndTitle(): PanelProps {
+            if (this.netProfit === undefined) {
+                return {
+                    name: 'notice',
+                    title: `The transaction net profit is unknown`,
+                };
+            }
+            if (this.netProfit.isLessThanOrEqualTo(0)) {
+                return {
+                    name: 'notice',
+                    title: `The transaction net profit is negative`,
+                };
+            }
+            return {
+                name: 'correct',
+                title: `The transaction net profit is positive`,
+            };
+        },
+    },
+    watch: {
+        currentStateAndTitle: {
+            immediate: true,
+            handler(newCurrentStateAndTitle) {
+                this.$emit('update:isCorrect', newCurrentStateAndTitle.name === 'correct');
+            },
+        },
+    },
+});
+</script>
