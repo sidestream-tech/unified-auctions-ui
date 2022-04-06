092c3a9168fe432b146c9af4ed00b1ca6201c1ad
Ben <benediktkaiser8@gmail.com>
2022-04-06 17:52:18 +0200

    feat: `GrossProfitCheckPanel` and `NetProfitCheckPanel` components (#191)

---
+++ frontend/components/panels/GrossProfitCheckPanel.vue
@@ -0,0 +1,55 @@
+<template>
+    <BasePanel :current-state="currentStateAndTitle.name">
+        <template #title> {{ currentStateAndTitle.title }} </template>
+        <TextBlock>
+            You cannot execute an auction while it is not yet profitable. Once the auction price drops below the price
+            on UniSwap, you may continue with the auction participation
+        </TextBlock>
+    </BasePanel>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import BigNumber from 'bignumber.js';
+import BasePanel from '../common/BasePanel.vue';
+import TextBlock from '../common/TextBlock.vue';
+
+export default Vue.extend({
+    name: 'GrossProfitCheckPanel',
+    components: { TextBlock, BasePanel },
+    props: {
+        grossProfit: {
+            type: Object as Vue.PropType<BigNumber>,
+            default: undefined,
+        },
+    },
+    computed: {
+        currentStateAndTitle(): PanelProps {
+            if (this.grossProfit === undefined) {
+                return {
+                    name: 'notice',
+                    title: `The transaction gross profit is unknown`,
+                };
+            }
+            if (this.grossProfit.isLessThanOrEqualTo(0)) {
+                return {
+                    name: 'incorrect',
+                    title: `The transaction gross profit is negative`,
+                };
+            }
+            return {
+                name: 'correct',
+                title: `The transaction gross profit is positive`,
+            };
+        },
+    },
+    watch: {
+        currentStateAndTitle: {
+            immediate: true,
+            handler(newCurrentStateAndTitle) {
+                this.$emit('update:isCorrect', newCurrentStateAndTitle.name === 'correct');
+            },
+        },
+    },
+});
+</script>
