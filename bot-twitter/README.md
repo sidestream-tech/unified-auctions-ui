55ea6459f1f5385915d0941b805a6b1e90726bd0
Valia Fetisov <contact@valiafetisov.com>
2022-04-12 09:37:59 +0200

    add MAX_PRIORITY_FEE_PER_GAS_WEI env variable, update readmes

--- bot-twitter/README.md
+++ bot-twitter/README.md
@@ -30,6 +30,7 @@ $ npm run start
 - `INFURA_PROJECT_ID`: (required) [infura](https://infura.io/) project id (can be found in: dashboard -> ethereum ->
   create new project -> settings -> keys). Note: this project can not be restricted by the origin.
 - `ETHEREUM_NETWORK`: (optional, default `kovan`) – internal network name on which the bot poll for auctions. Available
+- `MAX_PRIORITY_FEE_PER_GAS_WEI`: (optional, default can be found in core/src/gas.ts) – [EIP-1559](https://eips.ethereum.org/EIPS/eip-1559) `max_priority_fee_per_gas` value
   options can be found in [constants/NETWORKS](../core/src/constants/NETWORKS.ts)
 - `REFETCH_INTERVAL`: (optional, default 60 seconds) – interval between auction fetching requests
 - `KEEPER_*`: (optional) set of env variables to enable keeper bot:
--- core/src/gas.ts
+++ core/src/gas.ts
@@ -9,6 +9,18 @@ import getProvider from './provider';
 
 const CHAINLINK_ORACLE_ADDRESS = '0x169e633a2d1e6c10dd91238ba11c4a708dfef37c';
 const GAS_CACHE_MS = 10 * 1000;
+const maxPriorityFeePerGas = new BigNumber(process.env.MAX_PRIORITY_FEE_PER_GAS_WEI || '1500000000').shiftedBy(
+    -ETH_NUMBER_OF_DIGITS
+);
+
+export const getBaseFeePerGas = async function (network: string): Promise<BigNumber> {
+    const provider = await getProvider(network);
+    const pendingBlock = await provider.getBlock('latest');
+    if (!pendingBlock?.baseFeePerGas?._hex) {
+        return new BigNumber(NaN);
+    }
+    return new BigNumber(pendingBlock?.baseFeePerGas?._hex).shiftedBy(-ETH_NUMBER_OF_DIGITS);
+};
 
 const getOracleGasPrice = async function (network: string): Promise<BigNumber> {
     const provider = await getProvider(network);
@@ -20,8 +32,8 @@ const getOracleGasPrice = async function (network: string): Promise<BigNumber> {
 const _getEIP1559Values = async function (network: string): Promise<GasParameters> {
     const oracleGasPrice = await getOracleGasPrice(network);
     return {
-        maxFeePerGas: oracleGasPrice.shiftedBy(ETH_NUMBER_OF_DIGITS).toFixed(0),
-        maxPriorityFeePerGas: '1500000000',
+        maxPriorityFeePerGas: maxPriorityFeePerGas.shiftedBy(ETH_NUMBER_OF_DIGITS).toFixed(0),
+        maxFeePerGas: oracleGasPrice.plus(maxPriorityFeePerGas).shiftedBy(ETH_NUMBER_OF_DIGITS).toFixed(0),
     };
 };
 
