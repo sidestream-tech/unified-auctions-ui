65512da0c9a7c9fbdb0fe666be9244cbedddc35a
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-04-13 12:15:21 +0200

    Added Gas Store to retrieve all gas related information

---
+++ frontend/store/gas.ts
@@ -0,0 +1,80 @@
+import { ActionContext } from 'vuex';
+import type { GasParameters, TransactionFees } from 'auctions-core/src/types';
+import { getBaseFeePerGas, getGasParametersForTransaction } from 'auctions-core/src/gas';
+import BigNumber from 'bignumber.js';
+import { getApproximateTransactionFees } from 'auctions-core/src/fees';
+
+interface State {
+    baseFeePerGas: BigNumber | undefined;
+    gasParameters: GasParameters;
+    transactionFees: TransactionFees | undefined;
+}
+
+export const state = (): State => ({
+    baseFeePerGas: undefined,
+    gasParameters: {
+        maxFeePerGas: undefined,
+        maxPriorityFeePerGas: undefined,
+        gasPrice: undefined,
+    },
+    transactionFees: undefined,
+});
+
+export const getters = {
+    getBaseFeePerGas(state: State) {
+        return state.baseFeePerGas;
+    },
+    getGasParameters(state: State) {
+        return state.gasParameters;
+    },
+    getTransactionFees(state: State) {
+        return state.transactionFees;
+    },
+};
+
+export const mutations = {
+    setBaseFeePerGas(state: State, baseFeePerGas: BigNumber) {
+        state.baseFeePerGas = baseFeePerGas;
+    },
+    setGasParameters(state: State, gasParameters: GasParameters) {
+        state.gasParameters = {
+            ...state.gasParameters,
+            ...gasParameters,
+        };
+    },
+    setTransactionFees(state: State, transactionFees: TransactionFees) {
+        state.transactionFees = transactionFees;
+    },
+};
+
+export const actions = {
+    async fetchBaseFeePerGas({ commit, rootGetters }: ActionContext<State, State>) {
+        const network = rootGetters['network/getMakerNetwork'];
+        if (!network) {
+            return;
+        }
+        const baseFeePerGas = await getBaseFeePerGas(network);
+        commit('setBaseFeePerGas', baseFeePerGas);
+    },
+    async fetchGasParameters({ commit, rootGetters }: ActionContext<State, State>) {
+        const network = rootGetters['network/getMakerNetwork'];
+        if (!network) {
+            return;
+        }
+        const gasParameters = await getGasParametersForTransaction(network);
+        commit('setGasParameters', gasParameters);
+    },
+    async fetchTransactionFees({ commit, rootGetters }: ActionContext<State, State>) {
+        const network = rootGetters['network/getMakerNetwork'];
+        if (!network) {
+            return;
+        }
+        const transactionFees = await getApproximateTransactionFees(network);
+        commit('setTransactionFees', transactionFees);
+    },
+    async setup({ dispatch }: ActionContext<State, State>) {
+        await dispatch('fetchBaseFeePerGas');
+        await dispatch('fetchGasParameters');
+        await dispatch('fetchTransactionFees');
+    },
+};
