53e2b954b150aa8a607915bf65765fd5d685cf8a
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-04-14 06:54:58 +0200

    Updated Twitter Message

--- bot/src/notify.ts
+++ bot/src/notify.ts
@@ -3,7 +3,7 @@ import { sendNotification } from './twitter';
 
 const generateNotificationText = function (auction: AuctionInitialInfo): string {
     const url = `${process.env.FRONTEND_ORIGIN}/collateral/?network=${auction.network}&auction=${auction.id}`;
-    return `Collateral auction with ${auction.collateralAmount.toFixed(2)} ${
+    return `Collateral auction with ${auction.collateralAmount.toFixed(2)} $${
         auction.collateralSymbol
     } just started. Follow the link to participate: ${url}`;
 };
