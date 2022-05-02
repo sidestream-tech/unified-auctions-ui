d44de1c21ebb3bdab9d7d19b5e28e84056f74e43
Felix Egbert <fekiegbert@googlemail.com>
2022-05-02 17:23:42 +0200

    added env variable HEAPIO_ID

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -15,6 +15,7 @@ export default {
         GITHUB_URL: pkg.repository.url,
         STAGING_BANNER_URL: process.env.STAGING_BANNER_URL || undefined,
         DEFAULT_ETHEREUM_NETWORK: process.env.DEFAULT_ETHEREUM_NETWORK || 'mainnet',
+        HEAPIO_ID: process.env.HEAPIO_ID || undefined,
     },
 
     publicRuntimeConfig: {
