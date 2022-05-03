86863926a45a9e78046717a5836f4f2e9d136712
Felix Egbert <fekiegbert@googlemail.com>
2022-05-03 17:10:55 +0200

    Added SimulationLogs component

---
+++ frontend/components/simulation/SimulationLogs.stories.js
@@ -0,0 +1,34 @@
+import { storiesOf } from '@storybook/vue';
+import faker from 'faker';
+import SimulationLogs from './SimulationLogs';
+
+const randomLogs = length => {
+    return faker.datatype.array(length).map(() => {
+        return `${faker.date.recent().toISOString()}: ${faker.random.words(3)}`;
+    });
+};
+
+const common = {
+    components: { SimulationLogs },
+    data: () => ({
+        logs: randomLogs(5),
+    }),
+    template: '<SimulationLogs :logs="logs"/>',
+};
+
+storiesOf('Simulation/SimulationLogs', module)
+    .add('Default', () => ({
+        ...common,
+    }))
+    .add('Long logs', () => ({
+        ...common,
+        data: () => ({
+            logs: randomLogs(100),
+        }),
+    }))
+    .add('No logs', () => ({
+        ...common,
+        data: () => ({
+            logs: [],
+        }),
+    }));
