254c73b4a7838c4582b798529ab8085cf26c8600
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-05-04 15:46:49 +0200

    Updated how Websockets are opened

--- notification-service/src/websocket.ts
+++ notification-service/src/websocket.ts
@@ -2,7 +2,7 @@ import { ethers } from 'ethers';
 import { INFURA_PROJECT_ID, ETHEREUM_NETWORK } from './variables';
 import { getAbiFromContractAddress, SUBSCRIPTIONS } from './constants/SUBSCRIPTIONS';
 import { EVENT_PREFIX, WEBSOCKET_PREFIX } from './constants/PREFIXES';
-import { MailData } from './types';
+import { EventSubscription, MailData } from './types';
 
 export function setupWebSocket(): ethers.providers.WebSocketProvider {
     if (!INFURA_PROJECT_ID) {
@@ -18,24 +18,36 @@ export function setupWebSocket(): ethers.providers.WebSocketProvider {
 }
 
 export function subscribe(wsProvider: ethers.providers.WebSocketProvider, sendMail: (mailData: MailData) => void) {
-    SUBSCRIPTIONS.forEach(async subscription => {
-        const contractAbi = await getAbiFromContractAddress(subscription.contract);
-        const contract = new ethers.Contract(subscription.contract, contractAbi, wsProvider);
+    const contracts: Record<string, EventSubscription[]> = {};
+
+    SUBSCRIPTIONS.forEach(eventSubscription => {
+        if (!contracts[eventSubscription.contract]) {
+            contracts[eventSubscription.contract] = [eventSubscription];
+        } else {
+            contracts[eventSubscription.contract].push(eventSubscription);
+        }
+    });
+
+    Object.keys(contracts).forEach(async contractAddress => {
+        const contractAbi = await getAbiFromContractAddress(contractAddress);
+        const contract = new ethers.Contract(contractAddress, contractAbi, wsProvider);
 
         console.info(
-            `${WEBSOCKET_PREFIX} listening for event "${subscription.eventName}" on contract "${subscription.contract}"`
+            `${WEBSOCKET_PREFIX} registered websocket on contract "${contractAddress}" for ${contracts[contractAddress].length} event(s)`
         );
         contract.on('*', event => {
-            if (event.event === subscription.eventName) {
-                console.info(
-                    `${EVENT_PREFIX} event "${event.event}" triggered in block "${event.blockNumber}". Attempting to send email.`
-                );
-                sendMail({
-                    eventSubscription: subscription,
-                    eventData: event,
-                    formattedData: subscription.formatData(event),
-                });
-            }
+            contracts[contractAddress].forEach(eventSubscription => {
+                if (event.event === eventSubscription.eventName) {
+                    console.info(
+                        `${EVENT_PREFIX} event "${event.event}" triggered in block "${event.blockNumber}". Attempting to send email.`
+                    );
+                    sendMail({
+                        eventSubscription: eventSubscription,
+                        eventData: event,
+                        formattedData: eventSubscription.formatData(event),
+                    });
+                }
+            });
         });
     });
 }
