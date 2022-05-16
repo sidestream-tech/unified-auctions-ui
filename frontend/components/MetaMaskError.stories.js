904481b9db0431ceb5d8404b0a64feadab0b1381
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-05-16 15:22:54 +0200

    Fixed Linting

--- frontend/components/MetaMaskError.stories.js
+++ frontend/components/MetaMaskError.stories.js
@@ -2,17 +2,16 @@ import { storiesOf } from '@storybook/vue';
 import METAMASK_ERRORS from 'auctions-core/src/constants/METAMASK_ERRORS';
 import parseMetamaskError from 'auctions-core/src/helpers/parseMetamaskError';
 
-storiesOf('MetaMaskErrors', module)
-    .add('Plain', () => ({
-        data: () => ({
-            errors: METAMASK_ERRORS,
-        }),
-        methods: {
-            parseMetamaskError(error) {
-                return parseMetamaskError(error)
-            },
+storiesOf('MetaMaskErrors', module).add('Plain', () => ({
+    data: () => ({
+        errors: METAMASK_ERRORS,
+    }),
+    methods: {
+        parseMetamaskError(error) {
+            return parseMetamaskError(error);
         },
-        template: `<div class="flex flex-col space-y-2">
+    },
+    template: `<div class="flex flex-col space-y-2">
                 <div class="flex items-cent font-bold my-3">
                     <p>FormattedError</p>
                     <p class="mx-2">-</p>
@@ -24,4 +23,4 @@ storiesOf('MetaMaskErrors', module)
                     <p>{{OriginalError}}</p>
                 </div>
             </div>`,
-    }));
+}));
