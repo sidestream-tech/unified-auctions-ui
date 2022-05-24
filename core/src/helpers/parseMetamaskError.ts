4267d934737860c9fe305dbb9476e387e45b24e3
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-05-24 12:45:10 +0200

    New parsing method (WIP)

--- core/src/helpers/parseMetamaskError.ts
+++ core/src/helpers/parseMetamaskError.ts
@@ -1,8 +1,18 @@
 import truncateText from './truncateText';
 
 const parseMetamaskError = function (errorMessage = ''): unknown {
-    const splitError = errorMessage.split('(')[0];
-    return truncateText(splitError);
+    const jsonFirstIndex = errorMessage.indexOf('"message":');
+    const jsonLastIndex = errorMessage.lastIndexOf('"stack":');
+    const jsonString = `{${errorMessage.substring(jsonFirstIndex, jsonLastIndex - 1)}}`;
+
+    const messageFirstIndex = jsonString.indexOf("'{");
+    const messageLastIndex = jsonString.lastIndexOf("}'");
+
+    const messageJsonString = `${jsonString.substring(messageFirstIndex + 1, messageLastIndex)}}`;
+
+    console.log(messageJsonString)
+    const metamaskError = JSON.parse(messageJsonString);
+    return truncateText(metamaskError?.value?.data?.message || 'unknown');
 };
 
 export default parseMetamaskError;
