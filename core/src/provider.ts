8b157381ae4b983e6dd74ff6387ca2067661de70
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-06-07 14:24:05 +0200

    Added RPC Url parser to retrieve infura id

--- core/src/provider.ts
+++ core/src/provider.ts
@@ -1,10 +1,18 @@
 import { ethers } from 'ethers';
 import { getNetworkConfigByType } from './constants/NETWORKS';
+import { parseURLForInfuraId } from './rpcURL';
 
 const providers: Record<string, Promise<ethers.providers.BaseProvider>> = {};
 
-export const createProvider = async function (network: string, rpcUrl?: string) {
+export const createProvider = async function (network: string) {
+    const rpcUrl = process.env.RPC_URL;
     if (!rpcUrl) {
+        throw new Error('Please define a RPC URL from which to retrieve network data.');
+    }
+
+    const infuraProjectId = parseURLForInfuraId(rpcUrl);
+
+    if (infuraProjectId) {
         const networkConfig = getNetworkConfigByType(network);
         const provider = new ethers.providers.StaticJsonRpcProvider({ url: networkConfig.url });
         await provider.ready;
@@ -19,9 +27,9 @@ export const setProvider = function (network: string, provider: Promise<ethers.p
     providers[network] = provider;
 };
 
-const getProvider = function (network: string, rpcUrl?: string): Promise<ethers.providers.BaseProvider> {
+const getProvider = function (network: string): Promise<ethers.providers.BaseProvider> {
     if (!providers[network]) {
-        setProvider(network, createProvider(network, rpcUrl));
+        setProvider(network, createProvider(network));
     }
     return providers[network];
 };
---
+++ core/src/rpcURL.ts
@@ -0,0 +1,9 @@
+const INFURA_URL_PROJECT_ID_IDENTIFIER = 'v3/';
+
+export const parseURLForInfuraId = function (rpcUrl: string) {
+    if (!rpcUrl.includes('infura.io/v3')) {
+        return undefined;
+    }
+    const stringFirstIndex = rpcUrl.indexOf(INFURA_URL_PROJECT_ID_IDENTIFIER);
+    return rpcUrl.substring(stringFirstIndex + INFURA_URL_PROJECT_ID_IDENTIFIER.length) || undefined;
+};
