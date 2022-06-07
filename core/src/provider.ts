3d6eb5ed547111fb2fa3f4d45d09d5f525b7e9ea
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-06-07 12:59:38 +0200

    Added ability to test RPC URls

--- core/src/provider.ts
+++ core/src/provider.ts
@@ -1,9 +1,16 @@
 import { ethers } from 'ethers';
+import { getNetworkConfigByType } from './constants/NETWORKS';
 
 const providers: Record<string, Promise<ethers.providers.BaseProvider>> = {};
 
-export const createProvider = async function (rpcURL: string) {
-    const provider = new ethers.providers.StaticJsonRpcProvider({ url: rpcURL });
+export const createProvider = async function (network: string, rpcUrl?: string) {
+    if (!rpcUrl) {
+        const networkConfig = getNetworkConfigByType(network);
+        const provider = new ethers.providers.StaticJsonRpcProvider({ url: networkConfig.url });
+        await provider.ready;
+        return provider;
+    }
+    const provider = new ethers.providers.StaticJsonRpcProvider({ url: rpcUrl });
     await provider.ready;
     return provider;
 };
@@ -12,9 +19,9 @@ export const setProvider = function (network: string, provider: Promise<ethers.p
     providers[network] = provider;
 };
 
-const getProvider = function (network: string): Promise<ethers.providers.BaseProvider> {
+const getProvider = function (network: string, rpcUrl?: string): Promise<ethers.providers.BaseProvider> {
     if (!providers[network]) {
-        setProvider(network, createProvider(network));
+        setProvider(network, createProvider(network, rpcUrl));
     }
     return providers[network];
 };
