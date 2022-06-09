6c193a00cde1f993a2356065b3a5e735f3ac717a
Kirill Dogadin <kirillsc2@gmail.com>
2022-06-09 19:59:41 +0200

    `then` is gone

--- frontend/cypress/integration/basic-spec.ts
+++ frontend/cypress/integration/basic-spec.ts
@@ -28,47 +28,29 @@ describe('Collateral auctions', function () {
 
         cy.get('a').eq(1).should('contain.text', 'Participate').click();
         cy.get('div.justify-end').contains('Directly swap').click();
+        cy.get('div').contains('Authorize DAI Transactions').click();
         cy.get('div')
-            .contains('Authorize DAI Transactions')
-            .click()
-            .then(() =>
-                cy
-                    .get('div')
-                    .contains('Authorize ETH-C Transactions')
-                    .click({ timeout: 19 * 1000 })
-            );
+            .contains('Authorize ETH-C Transactions')
+            .click({ timeout: 19 * 1000 });
 
-        cy.contains(/^Execute$/)
-            .click({ timeout: 25 * 1000 })
-            .then(() => {
-                cy.get('svg.CloseIcon')
-                    .eq(1, { timeout: 10 * 1000 })
-                    .click();
-                cy.get('svg.CloseIcon')
-                    .first({ timeout: 10 * 1000 })
-                    .click();
-            });
+        cy.contains(/^Execute$/).click({ timeout: 25 * 1000 });
+        cy.get('svg.CloseIcon').eq(1).click();
+        cy.get('svg.CloseIcon').first().click();
     }
     function testBidWithDai() {
         cy.get('a').eq(2).should('contain.text', 'Participate').click();
         cy.contains(/^Bid with DAI$/).click();
-        cy.contains(/^Manage DAI in VAT$/)
-            .click()
-            .then(() => {
-                cy.contains('Allow unlimited access to DAI').click({ timeout: 8 * 1000 });
-                cy.contains(/^deposit$/).click();
-                cy.get('span.ant-modal-close-x').first().click();
-            });
+        cy.contains(/^Manage DAI in VAT$/).click();
+        cy.contains('Allow unlimited access to DAI').click({ timeout: 8 * 1000 });
+        cy.contains(/^deposit$/).click();
+        cy.get('span.ant-modal-close-x').first().click();
         cy.get('input.Input').first().type('100');
         cy.contains(/^Authorize .+ Transactions$/).click();
 
-        cy.contains('Bid 100')
-            .click({ timeout: 25 * 1000 })
-            .then(() => {
-                // close the popups and return to the main collateral view
-                cy.get('svg.CloseIcon').eq(1).click();
-                cy.get('svg.CloseIcon').first().click();
-            });
+        cy.contains('Bid 100').click({ timeout: 25 * 1000 });
+        // close the popups and return to the main collateral view
+        cy.get('svg.CloseIcon').eq(1).click();
+        cy.get('svg.CloseIcon').first().click();
     }
     it('participates with swap and with direct bid', function () {
         testSwapProfit();
