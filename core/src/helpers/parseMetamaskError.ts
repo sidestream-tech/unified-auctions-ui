56c2d0292a32b6a7fc0adf9b54b698a615a8e7ee
Valia Fetisov <contact@valiafetisov.com>
2022-06-09 18:14:30 +0200

    cleanup

--- core/src/helpers/parseMetamaskError.ts
+++ core/src/helpers/parseMetamaskError.ts
@@ -1,25 +1,42 @@
 import truncateText from './truncateText';
 
-const ERROR_MESSAGE_START_KEYWORD = '"message":"';
+const removeSpecialCharacters = function (text = '') {
+    return text.replace(/['\{\}\\"]/gm, '').replace(/\[.+\]/gm, '');
+};
+
+const getErrorDetails = function (errorMessage = '') {
+    // Get useful content between `"message":"` and `"`
+    const START_SYMBOLS = '"message":"';
+    const STOP_SYMBOLS = '"';
+    const jsonFirstIndex = errorMessage.indexOf(START_SYMBOLS);
+    if (jsonFirstIndex === -1) {
+        return '';
+    }
+    const messageStartIndex = jsonFirstIndex + START_SYMBOLS.length;
+    const messageStopIndex = errorMessage.indexOf(STOP_SYMBOLS, messageStartIndex);
+    const extractedContent = errorMessage.substring(messageStartIndex, messageStopIndex);
+    return removeSpecialCharacters(extractedContent);
+};
+
+const getErrorTitle = function (errorMessage = '') {
+    // Get useful content before code starts
+    const jsonStartIndex = errorMessage.search(/[\(\{]/gm);
+    if (jsonStartIndex === -1) {
+        return '';
+    }
+    return errorMessage.substring(0, jsonStartIndex).trim();
+};
 
 const parseMetamaskError = function (errorMessage = ''): unknown {
-    const jsonFirstIndex = errorMessage.indexOf(ERROR_MESSAGE_START_KEYWORD);
-    if (jsonFirstIndex === -1) {
-        return truncateText(errorMessage || 'unknown');
+    const errorTitle = getErrorTitle(errorMessage);
+    if (!errorTitle) {
+        return truncateText(errorMessage || 'Unknown error').trim();
     }
-    try {
-        const firstTrim = errorMessage.substring(jsonFirstIndex + ERROR_MESSAGE_START_KEYWORD.length);
-        const jsonLastIndex = firstTrim.indexOf('"');
-        const trimmedErrorMessage = firstTrim
-            .substring(0, jsonLastIndex)
-            .replace("'", '')
-            .replace('}', '')
-            .replace('{', '')
-            .replace('\\', '');
-        return truncateText(trimmedErrorMessage || 'unknown');
-    } catch {
-        return truncateText(errorMessage || 'unknown');
+    const errorDetails = getErrorDetails(errorMessage);
+    if (!errorDetails) {
+        return errorTitle;
     }
+    return `${errorDetails} (${errorTitle})`;
 };
 
 export default parseMetamaskError;
