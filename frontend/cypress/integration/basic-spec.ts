c126779ec4d386d5f6bf54cdcf9b1553f9df53a4
Kirill Dogadin <kirillsc2@gmail.com>
2022-06-09 17:02:19 +0200

    move the consts and rename vars

--- frontend/cypress/integration/basic-spec.ts
+++ frontend/cypress/integration/basic-spec.ts
@@ -1,27 +1,28 @@
 import { BigNumber } from 'ethers';
 import { expect } from 'chai';
 
+const HARDHAT_WALLET_PRIVATE_KEY = '0x701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82';
+const URL_TO_VISIT = `${process.env.FRONTEND_URL || 'localhost:3000'}/collateral?network=localhost`;
+
 describe('Collateral auctions', function () {
     // Set the private key to one of the deterministic hardhat keys.
     // see https://hardhat.org/hardhat-network#running-stand-alone-in-order-to-support-wallets-and-other-software
-    const KEEPER_WALLET_PRIVATE_KEY = '0x701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82';
-    const URL_TO_VISIT = `${process.env.FRONTEND_URL || 'localhost:3000'}/collateral?network=localhost`;
-    let daiBalanceBefore: Record<string, BigNumber>;
-    let daiBalanceAfter: Record<string, BigNumber>;
+    let walletBalanceBefore: Record<string, BigNumber>;
+    let walletBalanceAfter: Record<string, BigNumber>;
 
     function testSwapProfit() {
         cy.visit(URL_TO_VISIT);
         cy.window()
             .its('$nuxt')
             .then(function (nuxt) {
-                nuxt.$store.dispatch('wallet/createWalletFromPrivateKey', KEEPER_WALLET_PRIVATE_KEY);
+                nuxt.$store.dispatch('wallet/createWalletFromPrivateKey', HARDHAT_WALLET_PRIVATE_KEY);
             });
         cy.get('table').first({ timeout: 140000 }).should('contain.text', 'ETH');
 
         cy.window()
             .its('$nuxt')
             .then(function (nuxt) {
-                daiBalanceBefore = nuxt.$store.getters['wallet/walletBalances'];
+                walletBalanceBefore = nuxt.$store.getters['wallet/walletBalances'];
             });
 
         cy.get('a').eq(1).should('contain.text', 'Participate').click();
@@ -76,9 +77,9 @@ describe('Collateral auctions', function () {
         cy.window()
             .its('$nuxt')
             .then(function (nuxt) {
-                daiBalanceAfter = nuxt.$store.getters['wallet/walletBalances'];
+                walletBalanceAfter = nuxt.$store.getters['wallet/walletBalances'];
                 // eslint-disable-next-line no-unused-expressions
-                expect(daiBalanceAfter.walletVatDAI.gt(daiBalanceBefore.walletVatDAI)).to.be.true;
+                expect(walletBalanceAfter.walletVatDAI.gt(walletBalanceBefore.walletVatDAI)).to.be.true;
             });
     });
 });
