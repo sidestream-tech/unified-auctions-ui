6306b2b86e441c26df813db484714087f3277679
Kirill Dogadin <kirillsc2@gmail.com>
2022-06-10 17:46:00 +0200

    wip looking for the better assertation

--- frontend/cypress/integration/basic-spec.ts
+++ frontend/cypress/integration/basic-spec.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 const HARDHAT_WALLET_PRIVATE_KEY = '0x701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82';
 const URL_TO_VISIT = `${process.env.HOST || 'localhost'}:${process.env.PORT || '3000'}/collateral?network=localhost`;
 const PAGE_LOAD_TIMEOUT_MS = 140 * 1000;
-const CLICK_TIMEOUT = 15 * 1000;
+const CLICK_TIMEOUT = 30 * 1000;
 
 describe('Collateral auctions', function () {
     // Set the private key to one of the deterministic hardhat keys.
@@ -46,24 +46,32 @@ describe('Collateral auctions', function () {
         cy.contains('Allow unlimited access to DAI').click({ timeout: CLICK_TIMEOUT });
         cy.contains(/^deposit$/).click();
         cy.get('span.ant-modal-close-x').first().click();
-        cy.get('input.Input').first().type('100');
-        cy.contains(/^Authorize .+ Transactions$/).click();
-
-        cy.contains('Bid 100').click({ timeout: CLICK_TIMEOUT });
-        // close the popups and return to the main collateral view
-        cy.get('svg.CloseIcon').eq(1).click();
-        cy.get('svg.CloseIcon').first().click();
+        cy.wait(2000);
         cy.window()
             .its('$nuxt')
             .then(function (nuxt) {
                 walletBalanceAfter = nuxt.$store.getters['wallet/walletBalances'];
                 // eslint-disable-next-line no-unused-expressions
                 expect(walletBalanceAfter.walletVatDAI.gt(walletBalanceBefore.walletVatDAI)).to.be.true;
+                walletBalanceBefore = JSON.parse(JSON.stringify(walletBalanceAfter));
             });
+        cy.get('input.Input').first().type('100');
+        cy.contains(/^Authorize .+ Transactions$/).click();
+
+        cy.get('div.flex.flex-row-reverse').contains('Bid').click({ timeout: CLICK_TIMEOUT });
+        // close the popups and return to the main collateral view
+        cy.get('svg.CloseIcon').eq(1).click();
+        cy.get('svg.CloseIcon').first().click();
     }
     it('participates with swap and with direct bid', function () {
         testSwapProfit();
-        walletBalanceBefore = { ...walletBalanceAfter };
         testBidWithDai();
+        cy.window()
+            .its('$nuxt')
+            .then(function (nuxt) {
+                walletBalanceAfter = nuxt.$store.getters['wallet/walletBalances'];
+                // eslint-disable-next-line no-unused-expressions
+                expect(walletBalanceAfter.walletVatDAI.lt(walletBalanceBefore.walletVatDAI)).to.be.true;
+            });
     });
 });
