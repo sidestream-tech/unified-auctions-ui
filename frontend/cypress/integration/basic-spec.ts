72455fc3e631840dd4a3abfff23df91cb61f0b4f
Kirill Dogadin <kirillsc2@gmail.com>
2022-06-10 13:54:22 +0200

    standartize timeout, move the assertation of dai balance

--- frontend/cypress/integration/basic-spec.ts
+++ frontend/cypress/integration/basic-spec.ts
@@ -4,6 +4,7 @@ import { expect } from 'chai';
 const HARDHAT_WALLET_PRIVATE_KEY = '0x701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82';
 const URL_TO_VISIT = `${process.env.FRONTEND_URL || 'localhost:3000'}/collateral?network=localhost`;
 const PAGE_LOAD_TIMOUT_MS = 140 * 1000;
+const CLICK_TIMEOUT = 15 * 1000;
 
 describe('Collateral auctions', function () {
     // Set the private key to one of the deterministic hardhat keys.
@@ -29,11 +30,9 @@ describe('Collateral auctions', function () {
         cy.get('a').eq(1).should('contain.text', 'Participate').click();
         cy.get('div.justify-end').contains('Directly swap').click();
         cy.get('div').contains('Authorize DAI Transactions').click();
-        cy.get('div')
-            .contains('Authorize ETH-C Transactions')
-            .click({ timeout: 19 * 1000 });
+        cy.get('div').contains('Authorize ETH-C Transactions').click({ timeout: CLICK_TIMEOUT });
 
-        cy.contains(/^Execute$/).click({ timeout: 25 * 1000 });
+        cy.contains(/^Execute$/).click({ timeout: CLICK_TIMEOUT });
         cy.get('svg.CloseIcon').eq(1).click();
         cy.get('svg.CloseIcon').first().click();
     }
@@ -41,20 +40,16 @@ describe('Collateral auctions', function () {
         cy.get('a').eq(2).should('contain.text', 'Participate').click();
         cy.contains(/^Bid with DAI$/).click();
         cy.contains(/^Manage DAI in VAT$/).click();
-        cy.contains('Allow unlimited access to DAI').click({ timeout: 8 * 1000 });
+        cy.contains('Allow unlimited access to DAI').click({ timeout: CLICK_TIMEOUT });
         cy.contains(/^deposit$/).click();
         cy.get('span.ant-modal-close-x').first().click();
         cy.get('input.Input').first().type('100');
         cy.contains(/^Authorize .+ Transactions$/).click();
 
-        cy.contains('Bid 100').click({ timeout: 25 * 1000 });
+        cy.contains('Bid 100').click({ timeout: CLICK_TIMEOUT });
         // close the popups and return to the main collateral view
         cy.get('svg.CloseIcon').eq(1).click();
         cy.get('svg.CloseIcon').first().click();
-    }
-    it('participates with swap and with direct bid', function () {
-        testSwapProfit();
-        testBidWithDai();
         cy.window()
             .its('$nuxt')
             .then(function (nuxt) {
@@ -62,5 +57,10 @@ describe('Collateral auctions', function () {
                 // eslint-disable-next-line no-unused-expressions
                 expect(walletBalanceAfter.walletVatDAI.gt(walletBalanceBefore.walletVatDAI)).to.be.true;
             });
+    }
+    it('participates with swap and with direct bid', function () {
+        testSwapProfit();
+        walletBalanceBefore = { ...walletBalanceAfter };
+        testBidWithDai();
     });
 });
