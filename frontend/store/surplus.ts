f394bfada50cf5216733aaeaf365cb470e4c095a
Kirill Dogadin <kirillsc2@gmail.com>
2022-06-23 12:16:46 +0200

    Add store functionality

--- frontend/store/surplus.ts
+++ frontend/store/surplus.ts
@@ -1,38 +1,36 @@
-import type { AuctionTransaction, TakeEvent } from 'auctions-core/src/types';
+import type { SurplusAuctionFullData } from 'auctions-core/src/types';
 import { ActionContext } from 'vuex';
 import { fetchActiveSurplusAuctions } from 'auctions-core/src/surplus';
 
 interface State {
-    auctionStorage: Record<string, AuctionTransaction>;
-    takeEventStorage: Record<string, TakeEvent[]>;
-    areAuctionsFetching: boolean;
-    isSelectedAuctionFetching: boolean;
-    areTakeEventsFetching: boolean;
-    isBidding: boolean;
-    error: string | null;
-    auctionErrors: Record<string, string | undefined>;
-    restartingAuctionsIds: string[];
-    lastUpdated: Date | undefined;
+    auctionStorage: Record<string, SurplusAuctionFullData>;
 }
 
 const getInitialState = (): State => ({
     auctionStorage: {},
-    takeEventStorage: {},
-    areAuctionsFetching: false,
-    isSelectedAuctionFetching: false,
-    areTakeEventsFetching: false,
-    isBidding: false,
-    error: null,
-    auctionErrors: {},
-    restartingAuctionsIds: [],
-    lastUpdated: undefined,
 });
 
 export const state = (): State => getInitialState();
+
+export const getters = {
+    auctionStorage(state: State) {
+        return state.auctionStorage;
+    }
+}
+
+export const mutations = {
+    addAuctionToStorage(state: State, auction: SurplusAuctionFullData) {
+        state.auctionStorage[auction.id] = auction
+    }
+}
+
 export const actions = {
-    async fetchSurplusAuctions({ rootGetters }: ActionContext<State, State>) {
+    async fetchSurplusAuctions({ rootGetters, commit }: ActionContext<State, State>) {
         const network = rootGetters['network/getMakerNetwork'];
         const auctions = await fetchActiveSurplusAuctions(network);
+        auctions.forEach(
+            auction => commit('surplus/addAuctionToStorage', auction)
+        )
         return auctions;
     },
 };
