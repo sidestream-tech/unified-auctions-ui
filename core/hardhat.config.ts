b34b9e62f435d8467b964a9d783968c8f1ad31c9
Kirill Dogadin <kirillsc2@gmail.com>
2022-06-30 09:26:55 +0200

    add simple test to the core

---
+++ core/hardhat.config.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import "@nomiclabs/hardhat-waffle";
+
+dotenv.config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+export default {
+  solidity: "0.8.4",
+};
---
+++ core/test/some-test.ts
@@ -0,0 +1,23 @@
+import { expect } from "chai";
+import { fetchActiveSurplusAuctions } from "../src/surplus";
+import hre from "hardhat";
+
+const ALCHEMY_URL = process.env.ALCHEMY_URL;
+
+describe("Some", () => {
+  it("tests", async () => {
+    await hre.network.provider.request({
+      method: "hardhat_reset",
+      params: [
+        {
+          forking: {
+            jsonRpcUrl: ALCHEMY_URL,
+            blockNumber: 14078339,
+          },
+        },
+      ],
+    });
+    const contracts = await fetchActiveSurplusAuctions("localhost");
+    expect(contracts.length).to.equal(5);
+  });
+});
