c367c48944c1a80845585d5d240fcad9b9260e4c
Kirill Dogadin <kirillsc2@gmail.com>
2022-06-30 16:05:37 +0200

    adjust config hardhat

--- core/hardhat.config.ts
+++ core/hardhat.config.ts
@@ -7,14 +7,16 @@ const NETWORK_HOST = process.env.NETWORK_HOST || "localhost";
 const NETWORK_PORT = process.env.NETWORK_PORT || "8545";
 const LOCAL_NETWORK_URL = `http://${NETWORK_HOST}:${NETWORK_PORT}`;
 
+const customNetworkConfig = {
+        [NETWORK_HOST]: {
+            url: LOCAL_NETWORK_URL
+        }
+}
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 export default {
     solidity: '0.8.4',
-    networks: {
-        NETWORK: {
-            url: LOCAL_NETWORK_URL
-        }
-    }
+    networks: customNetworkConfig,
 };
