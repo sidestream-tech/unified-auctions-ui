eab8083145a2063884ea1c1d8ab584de0542f80c
Abdelrhman Farag <aomafarag@gmail.com>
2022-06-30 13:55:21 +0200

    Initial implementation of component and stories

---
+++ frontend/components/SurplusAuction.stories.ts
@@ -0,0 +1,47 @@
+import type { Auction } from 'auctions-core/src/types';
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import SurplusAuction from './SurplusAuction.vue';
+import { generateFakeAuction } from '~/helpers/generateFakeAuction';
+
+const fakeAuction = generateFakeAuction();
+const basicStory = {
+    components: {
+        Auction: SurplusAuction,
+    },
+    computed: {
+        auctionParams(): Auction {
+            return {
+                ...fakeAuction,
+            };
+        },
+    },
+    data() {
+        return {
+            fakeAuction,
+        };
+    },
+    methods: {
+        swap: action('swap'),
+    },
+};
+
+storiesOf('Auction', module)
+    .add('Default', () => ({
+        ...basicStory,
+        template: `<Auction :auction="auctionParams" auctionId="test" @swap="swap"/>`,
+    }))
+    .add('Max Width', () => ({
+        ...basicStory,
+        template: `
+        <div class="flex items-center w-3/5">
+            <Auction :auction="auctionParams" auctionId="test" @swap="swap"/>
+        </div>`,
+    }))
+    .add('Finished', () => ({
+        ...basicStory,
+        template: `
+        <div class="flex items-center w-3/5">
+            <Auction :auction="auctionParams" auctionId="test" error="This auction is finished"/>
+        </div>`,
+    }));
