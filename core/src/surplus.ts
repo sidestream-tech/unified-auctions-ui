a0bb56e58287f29c7f69a8ff6e2b5d9e52a617d2
Kirill Dogadin <kirillsc2@gmail.com>
2022-07-04 15:18:38 +0200

    remove test-fixture function from src

--- core/src/surplus.ts
+++ core/src/surplus.ts
@@ -6,13 +6,7 @@ import { Contract } from 'ethers';
 import getNetworkDate from './date';
 import { RAD, WAD, WAD_NUMBER_OF_DIGITS, RAD_NUMBER_OF_DIGITS } from './constants/UNITS';
 import executeTransaction from './execute';
-import getSigner from './signer';
-import { getGasParametersForTransaction } from './gas';
-import trackTransaction from './tracker';
 import { getNetworkConfigByType } from './constants/NETWORKS';
-import WETH from './abis/WETH.json';
-import { ethers } from 'hardhat';
-import UNISWAP from './abis/UNISWAP.json';
 
 const getSurplusAuctionLastIndex = async (contract: Contract): Promise<number> => {
     const auctionsQuantityBinary = await contract.kicks();
@@ -144,62 +138,3 @@ export const collectSurplusAuction = async function (network: string, auctionInd
     await executeTransaction(network, 'MCD_FLAP', 'deal', [auctionIndex], notifier);
 };
 
-const canTransactionBeConfirmed = function (network: string, confirmTransaction?: boolean) {
-    const networkConfig = getNetworkConfigByType(network);
-    if (networkConfig.isFork) {
-        return false;
-    }
-    return confirmTransaction;
-};
-export const swapToMKR = async function (
-    network: string,
-    amountPaidAllowanceETH: string | number,
-    amountReceivedMinMKR: string | number,
-    notifier?: Notifier
-) {
-    const signer = await getSigner(network);
-    const address = await signer.getAddress();
-    const WETH_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-    const UNISWAP_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D';
-    const MKR_ADDRESS = '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2';
-    const contractWeth = await new Contract(WETH_ADDRESS, WETH, signer);
-    const contractUniswap = await new Contract(UNISWAP_ADDRESS, UNISWAP, signer);
-
-    // Allow operations with the uniswap to swap from weth
-    const gasParameters = await getGasParametersForTransaction(network);
-    let transactionPromise = contractWeth['approve'](
-        ...[UNISWAP_ADDRESS, new BigNumber(amountPaidAllowanceETH).shiftedBy(WAD_NUMBER_OF_DIGITS).toFixed(0)],
-        {
-            ...gasParameters,
-            type: gasParameters.gasPrice ? undefined : 2,
-        }
-    );
-
-    await trackTransaction(transactionPromise, notifier, canTransactionBeConfirmed(network));
-
-    //Get some eth
-    await trackTransaction(
-        contractWeth.deposit({
-            value: ethers.utils.parseEther(String(amountPaidAllowanceETH)),
-        }),
-        notifier,
-        canTransactionBeConfirmed(network)
-    );
-
-    // get some mkr
-    const deadline = await getNetworkDate(network);
-    deadline.setHours(deadline.getDate() + 1);
-    const transactionParamsSwapMkr = [
-        new BigNumber(amountReceivedMinMKR).shiftedBy(WAD_NUMBER_OF_DIGITS).toFixed(0),
-        [WETH_ADDRESS, MKR_ADDRESS],
-        address,
-        new BigNumber(Math.floor(deadline.getTime() / 1000)).toFixed(0),
-    ];
-    const gasParametersSwapMKR = await getGasParametersForTransaction(network);
-    transactionPromise = contractUniswap['swapETHForExactTokens'](...transactionParamsSwapMkr, {
-        ...gasParametersSwapMKR,
-        type: gasParametersSwapMKR.gasPrice ? undefined : 2,
-        value: new BigNumber(amountPaidAllowanceETH).shiftedBy(WAD_NUMBER_OF_DIGITS).toFixed(0),
-    });
-    await trackTransaction(transactionPromise, notifier, canTransactionBeConfirmed(network));
-};
