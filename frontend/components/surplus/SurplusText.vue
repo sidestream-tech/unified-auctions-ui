77472de872877f98553e8161d7af757ea957f981
Abdelrhman Farag <aomafarag@gmail.com>
2022-07-11 10:36:32 +0200

    Remove WhatIsCatch component

--- frontend/components/surplus/SurplusText.vue
+++ frontend/components/surplus/SurplusText.vue
@@ -26,7 +26,19 @@
             the DAI auctioned off can be collected by the highest bidder. As long as the auction price is below the
             exchange rate on other marketplaces there is a chance to make a profit.
         </TextBlock>
-        <WhatIsCatch v-if="isExplanationsShown" class="TextBlock"></WhatIsCatch>
+        <TextBlock v-if="isExplanationsShown" title="What is the catch?" class="TextBlock">
+            This situation exists in the first place, because the Maker protocol can not be executed by itself. There
+            need to be players who execute vital parts of the protocol and pay a
+            <Explain text="transaction fee">
+                Transaction fees are paid as <a href="https://ethereum.org/en/developers/docs/gas/">Gas</a> for the
+                Ethereum network. It is the fuel that allows it to operate, in the same way, that a car needs gasoline
+                to run.
+            </Explain>
+            for it. In a decentralised system like this, anyone can become such a player. But as execution can
+            sometimes end up in a loss of transaction fee, those operations are made to be profitable by the protocol
+            itself. Most of the auctions will be cleared by players with bots bidding very quickly, whenever there is a
+            slight market opportunity.
+        </TextBlock>
     </div>
 </template>
 
@@ -35,14 +47,12 @@ import Vue from 'vue';
 import WhatIsMakerProtocol from '~/components/WhatIsMakerProtocol.vue';
 import TextBlock from '~/components/common/TextBlock.vue';
 import Explain from '~/components/utils/Explain.vue';
-import WhatIsCatch from '~/components/WhatIsCatch.vue';
 
 export default Vue.extend({
     components: {
         WhatIsMakerProtocol,
         TextBlock,
         Explain,
-        WhatIsCatch,
     },
     props: {
         isExplanationsShown: {
