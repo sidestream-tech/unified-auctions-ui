0e460b258a58ef6ce0aa6af9c1ce4dfef44d01d3
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-07-12 13:44:19 +0200

    Reverted to old file

--- frontend/components/utils/FormatMarketValue.vue
+++ frontend/components/utils/FormatMarketValue.vue
@@ -1,5 +1,5 @@
 <template>
-    <span v-if="value !== undefined" :class="{ 'text-primary dark:text-primary-light': isBelowZero || !isBelowZero }"
+    <span v-if="value !== undefined" :class="{ 'text-primary dark:text-primary-light': isBelowZero }"
         ><animated-number :value="percentage" />% {{ belowOrAbove }}</span
     >
 </template>
