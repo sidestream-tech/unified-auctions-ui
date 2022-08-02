6d92784ce7b77a1658148425bb9b825a3efc242f
Abdelrhman Farag <aomafarag@gmail.com>
2022-08-02 12:04:40 +0200

    Add under in tweet when value is small

--- bot/src/notify.ts
+++ bot/src/notify.ts
@@ -1,12 +1,16 @@
 import type { AuctionInitialInfo } from 'auctions-core/src/types';
-import { formatToAutomaticDecimalPoints } from 'auctions-core/src/helpers/formatToAutomaticDecimalPoints';
+import {
+    isValueSmallButNotZero,
+    formatToAutomaticDecimalPoints,
+} from 'auctions-core/src/helpers/formatToAutomaticDecimalPoints';
 import { sendNotification } from './twitter';
 
 const generateNotificationText = function (auction: AuctionInitialInfo): string {
     const url = `${process.env.FRONTEND_ORIGIN}/collateral/?network=${auction.network}&auction=${auction.id}`;
-    return `Collateral auction with ${formatToAutomaticDecimalPoints(auction.collateralAmount)} ${
-        auction.collateralSymbol
-    } just started. Follow the link to participate: ${url}`;
+    const formattedValue = formatToAutomaticDecimalPoints(auction.collateralAmount);
+    const displayValue = isValueSmallButNotZero(auction.collateralAmount) ? `under ${formattedValue}` : formattedValue;
+
+    return `Collateral auction with ${displayValue} ${auction.collateralSymbol} just started. Follow the link to participate: ${url}`;
 };
 
 const notify = async function (auction: AuctionInitialInfo) {
