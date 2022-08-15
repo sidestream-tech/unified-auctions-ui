8780b6b37c6e7a5063c306653823017d75d54139
Kirill Dogadin <kirillsc2@gmail.com>
2022-08-15 16:18:34 +0200

    add const files

---
+++ core/helpers/constants.ts
@@ -0,0 +1,5 @@
+// Values used in tests and external scripts.
+
+export const HARDHAT_PRIVATE_KEY = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'; // deterministic private key from hardhat.
+export const NETWORK = 'custom';
+export const HARDHAT_FORK_BLOCK_NUMBER = 14052140;
