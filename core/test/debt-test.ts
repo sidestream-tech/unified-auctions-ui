069e0bcb7603aef3512bdb69fc493433723065ef
Kirill Dogadin <kirillsc2@gmail.com>
2022-08-19 16:02:19 +0200

    adjust test

--- core/test/debt-test.ts
+++ core/test/debt-test.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai';
-import { causeDebt } from '../helpers/causeDebt';
+import { causeDebt } from '../simulation/scripts/causeDebt';
 import getContract from '../src/contracts';
-import BigNumber from '../src/bignumber';
 import { HARDHAT_PRIVATE_KEY, NETWORK, REMOTE_RPC_URL } from '../helpers/constants';
 import { resetBlockchainFork as reset } from '../helpers/hardhat';
 const HARDHAT_FORK_BLOCK_NUMBER = 14052140;
@@ -9,7 +8,10 @@ const HARDHAT_FORK_BLOCK_NUMBER = 14052140;
 describe('Debt Auction', () => {
     beforeEach(async () => {
         const provider = await reset(HARDHAT_FORK_BLOCK_NUMBER, HARDHAT_PRIVATE_KEY, NETWORK, REMOTE_RPC_URL);
-        await causeDebt(NETWORK, provider, new BigNumber(10), new BigNumber(1000), new BigNumber(1000));
+        await causeDebt(
+            { network: NETWORK, debtAmountDai: '10', mkrOnAuction: '1000', daiOnAuction: '1000' },
+            provider
+        );
     });
     it('Flopper kicked', async () => {
         const debtContract = await getContract(NETWORK, 'MCD_FLOP');
