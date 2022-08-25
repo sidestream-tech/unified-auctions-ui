04699aff7975cc6ceeaae10d9b84084edea55de9
Kirill Dogadin <kirillsc2@gmail.com>
2022-08-25 12:08:47 +0200

    adjust the provided args

--- core/scripts/causeDebt.ts
+++ core/scripts/causeDebt.ts
@@ -1,6 +1,5 @@
 import hre from 'hardhat';
 import { causeDebt } from '../helpers/causeDebt';
-import BigNumber from '../src/bignumber';
 import { HARDHAT_PRIVATE_KEY } from '../helpers/constants';
 import { createWalletForRpc } from '../helpers/hardhat';
 
@@ -8,5 +7,5 @@ const provider = hre.network.provider;
 
 (async () => {
     await createWalletForRpc(HARDHAT_PRIVATE_KEY, 'custom');
-    await causeDebt('custom', provider, new BigNumber(10), new BigNumber(1000), new BigNumber(1000));
+    await causeDebt('custom', provider);
 })();
