33495b17dbd2fc729f73b1bf9f6ccbb86db35230
Kirill Dogadin <kirillsc2@gmail.com>
2022-08-27 17:05:49 +0200

    adjust env var

--- core/hardhat.config.ts
+++ core/hardhat.config.ts
@@ -5,10 +5,10 @@ import ethers from '@nomiclabs/hardhat-ethers';
 dotenv.config();
 
 const LOCAL_RPC_URL = process.env.LOCAL_RPC_URL || 'http://localhost:8545';
-const ALCHEMY_URL = process.env.ALCHEMY_URL;
+const REMOTE_RPC_URL = process.env.REMOTE_RPC_URL;
 
-if (!ALCHEMY_URL) {
-    throw new Error('ALCHEMY_URL env variable not set, please check your `hardhat/.env` file');
+if (!REMOTE_RPC_URL) {
+    throw new Error('REMOTE_RPC_URL env variable not set, please check your `hardhat/.env` file');
 }
 
 const customNetworkConfig = {
@@ -18,7 +18,7 @@ const customNetworkConfig = {
     hardhat: {
         chainId: 1337,
         forking: {
-            url: ALCHEMY_URL,
+            url: REMOTE_RPC_URL,
             // Few blocks before WSTETH-A is taken at 14052147,
             // https://etherscan.io/address/0x49a33a28c4c7d9576ab28898f4c9ac7e52ea457a
             blockNumber: 14052140,
