0716b1de59b29a36873d390ae56c3d944f0fa107
Kirill Dogadin <kirillsc2@gmail.com>
2022-09-05 15:36:18 +0200

    rename network

--- core/local.hardhat.config.ts
+++ core/local.hardhat.config.ts
@@ -7,7 +7,7 @@ dotenv.config();
 const LOCAL_RPC_URL = process.env.LOCAL_RPC_URL || 'http://localhost:8545';
 
 const customNetworkConfig = {
-    testnetwork: {
+    hardhat: {
         url: LOCAL_RPC_URL,
     },
 };
