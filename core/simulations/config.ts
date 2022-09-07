da0731b7ffe8e0ba425c2c31f9d0fc0fd101e550
Kirill Dogadin <kirillsc2@gmail.com>
2022-09-07 15:54:04 +0200

    add simulation types

--- core/simulations/config.ts
+++ core/simulations/config.ts
@@ -3,7 +3,16 @@ import surplusAuctionBlockchain from './configs/surplusAuctionSimulation';
 import wstethAuctionSimulation from './configs/wstethAuctionSimulation';
 import surplusAuctionSimulation from './configs/createSurplusAcutionSimulation';
 
-export const SIMULATIONS = [
+interface SimulationStep {
+    title: string;
+    entry: CallableFunction
+}
+interface Simulation {
+    readonly title: string;
+    readonly steps: ReadonlyArray<SimulationStep>;
+}
+
+export const SIMULATIONS: Simulation[] = [
     debtAuctionSimulation,
     surplusAuctionBlockchain,
     surplusAuctionSimulation,
