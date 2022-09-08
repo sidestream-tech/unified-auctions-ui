308d2ad92080cc9a02085556bb954fb10107b4c0
Kirill Dogadin <kirillsc2@gmail.com>
2022-09-08 17:21:56 +0200

    Add limit fetch

--- core/src/vaults.ts
+++ core/src/vaults.ts
@@ -1,5 +1,5 @@
 import getContract from './contracts';
-import { VaultBase, CollateralType } from './types';
+import { VaultBase, CollateralType, VaultAmount, VaultCollateralParameters } from './types';
 import BigNumber from './bignumber';
 import { ethers } from 'ethers';
 
@@ -22,10 +22,48 @@ export const fetchVaultsCount = async (network: string): Promise<BigNumber> => {
     return new BigNumber(countHex._hex);
 };
 
-export const fetchVatVault = async (network: string, type: CollateralType, vaultAddress: string) => {
+export const fetchVaultCollateralParameters = async (
+    network: string,
+    type: CollateralType
+): Promise<VaultCollateralParameters> => {
     const contract = await getContract(network, 'MCD_VAT');
     const typeHex = ethers.utils.formatBytes32String(type);
-    const {ink, art} = await contract.urns(typeHex, vaultAddress);
-    const {Art, rate, spot, line, dust} = await contract.ilks(typeHex);
-    console.log(ink, art, Art, rate, spot, line, dust);
+    const { rate, spot } = await contract.ilks(typeHex);
+    return {
+        stabilityFeeRate: rate,
+        minUnitPrice: spot,
+    };
+};
+
+export const fetchVaultAmount = async (
+    network: string,
+    type: CollateralType,
+    vaultAddress: string
+): Promise<VaultAmount> => {
+    const contract = await getContract(network, 'MCD_VAT');
+    const typeHex = ethers.utils.formatBytes32String(type);
+    const { ink, art } = await contract.urns(typeHex, vaultAddress);
+    return {
+        initialDebtDai: art,
+        collateralAmount: ink,
+    };
+};
+
+export const fetchGlobalLiquidationLimits = async (network: string) => {
+    const contract = await getContract(network, 'MCD_DOG');
+    const maximumProtocolDebtDaiHex = await contract.Hole();
+    const maximumProtocolDebtDai = new BigNumber(maximumProtocolDebtDaiHex._hex);
+    const currentProtocolDebtDaiHex = await contract.Hole();
+    const currentProtocolDebtDai = new BigNumber(currentProtocolDebtDaiHex._hex);
+    return {
+        currentProtocolDebtDai,
+        maximumProtocolDebtDai,
+    };
+};
+
+export const fetchCollateralLiquidationLimits = async (network: string, type: string) => {
+    const contract = await getContract(network, 'MCD_DOG');
+    const typeHex = ethers.utils.formatBytes32String(type);
+    const { hole, dirt } = await contract.ilks(typeHex);
+    return { currentCollateralDebtDai: dirt, maximumCollateralDebtDai: hole };
 };
