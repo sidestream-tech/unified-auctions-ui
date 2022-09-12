4ede1ec2bf8fa5ce24ef407a1d2b12123e42ee57
Abdelrhman Farag <85403429+aomafarag@users.noreply.github.com>
2022-09-12 15:11:51 +0200

    feat: New `AnimatedArrow` component (#444)

---
+++ frontend/components/common/other/AnimatedArrow.vue
@@ -0,0 +1,40 @@
+<template>
+    <UpArrow :style="style" />
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import UpArrow from '~/assets/icons/uparrow.svg';
+
+type ArrowDirections = 'up' | 'down' | 'left' | 'right';
+
+export default Vue.extend({
+    components: {
+        UpArrow,
+    },
+    props: {
+        direction: {
+            type: String as Vue.PropType<ArrowDirections>,
+            required: true,
+        },
+    },
+    computed: {
+        style(): Object {
+            let rotationDegree = 0;
+            if (this.direction === 'down') {
+                rotationDegree = 180;
+            }
+            if (this.direction === 'left') {
+                rotationDegree = -90;
+            }
+            if (this.direction === 'right') {
+                rotationDegree = 90;
+            }
+            return {
+                transform: `rotate(${rotationDegree}deg)`,
+                transition: 'transform 0.5s linear',
+            };
+        },
+    },
+});
+</script>
