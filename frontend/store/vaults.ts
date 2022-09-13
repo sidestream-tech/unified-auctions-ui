73c839b32272c6a4eb70dc7738e0b470f7f8ccca
Kirill Dogadin <kirillsc2@gmail.com>
2022-09-13 12:26:30 +0200

    store liquidation of a vault

--- frontend/store/vaults.ts
+++ frontend/store/vaults.ts
@@ -1,6 +1,6 @@
 import { Vault, VaultTransaction } from 'auctions-core/src/types';
 import Vue from 'vue';
-import { fetchVault, fetchVaultsCount } from 'auctions-core/src/vaults';
+import { fetchVault, fetchVaultsCount, liquidateVault } from 'auctions-core/src/vaults';
 import { ActionContext } from 'vuex';
 
 interface State {
@@ -17,7 +17,11 @@ const getInitialState = (): State => ({
 
 export const state = (): State => getInitialState();
 
-export const getters = {};
+export const getters = {
+    vaultTransactions(state: State) {
+        return state.vaultTransactions;
+    },
+};
 export const mutations = {
     setVault(state: State, vaultTransaction: VaultTransaction) {
         Vue.set(state, 'vaults', vaultTransaction);
@@ -50,6 +54,19 @@ export const actions = {
             commit('setVaultsCount', vaultCount);
         } catch (e) {
             console.error(`Failed to fetch vault count: ${e}`);
+        } finally {
+            commit('setIsVaultLoading', false);
+        }
+    },
+    async liquidateVault({ rootGetters, commit, getters }: ActionContext<State, State>, vaultId: number) {
+        const network = rootGetters['network/getMakerNetwork'];
+        const walletAddress = rootGetters['wallet/getAddress'];
+        const vaultTransaction = getters.vaultTransactions[vaultId];
+        commit('setIsVaultLoading', true);
+        try {
+            await liquidateVault(network, vaultTransaction, walletAddress);
+        } catch (e) {
+            console.error(`Failed to liquidate vault ${vaultId}: ${e}`);
         } finally {
             commit('setIsVaultLoading', false);
         }
