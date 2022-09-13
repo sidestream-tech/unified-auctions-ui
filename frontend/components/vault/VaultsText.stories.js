0f4bf29a1abcb4d1e3a9f622723604e054ef82a9
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-09-13 10:05:22 +0200

    WIP: Added VaultsText Component

---
+++ frontend/components/vault/VaultsText.stories.js
@@ -0,0 +1,43 @@
+import { storiesOf } from '@storybook/vue';
+import faker from 'faker';
+import VaultsText from './VaultsText.vue';
+import { generateFakeVaultTransactions } from '~/helpers/generateFakeVault.ts';
+
+const fakeVaults = generateFakeVaultTransactions();
+const randomSelectedVault = faker.random.arrayElement(fakeVaults);
+
+const common = {
+    components: { VaultsText },
+    data: () => ({
+        vaultTransactions: fakeVaults,
+        selectedVaultId: randomSelectedVault.id,
+    }),
+};
+
+storiesOf('Vault/VaultsText', module)
+    .add('Plain', () => ({
+        ...common,
+        template: '<VaultsText :vaultTransactions="vaultTransactions" :selectedVaultId="selectedVaultId" />',
+    }))
+    .add('Fetching with Auctions', () => ({
+        ...common,
+        template:
+            '<VaultsText :vaultTransactions="vaultTransactions" :selectedVaultId="selectedVaultId" :isVaultsLoading="true" />',
+    }))
+    .add('Fetching without Auctions', () => ({
+        ...common,
+        template: '<VaultsText :isVaultsLoading="true" />',
+    }))
+    .add('Error', () => ({
+        ...common,
+        template: '<VaultsText vaultsError="There was an error fetching the Vaults." />',
+    }))
+    .add('Expert Mode', () => ({
+        ...common,
+        template:
+            '<VaultsText :vaultTransactions="vaultTransactions" :selectedVaultId="selectedVaultId" :isExplanationsShown="false" />',
+    }))
+    .add('No props', () => ({
+        ...common,
+        template: '<VaultsText />',
+    }));
---
+++ frontend/components/vault/VaultsText.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="flex flex-col space-y-8 py-8">
+        <WhatIsMakerProtocol v-if="isExplanationsShown" class="TextBlock"></WhatIsMakerProtocol>
+        <template v-if="isExplanationsShown">
+            <TextBlock title="What are vaults?" class="TextBlock"> TODO </TextBlock>
+            <TextBlock title="When are vaults at risk?" class="TextBlock"> TODO </TextBlock>
+            <TextBlock v-if="vaultTransactions.length > 0" title="Vaults at risk" class="TextBlock w-full">
+                TODO
+            </TextBlock>
+            <TextBlock v-else title="No at risk vaults" class="TextBlock"> TODO </TextBlock>
+        </template>
+        <div
+            class="w-full self-center"
+            :class="{ 'max-w-4xl': isExplanationsShown, 'md:px-10': !isExplanationsShown }"
+        >
+            <!-- Add VaultsTable -->
+        </div>
+        <TextBlock v-if="isExplanationsShown" title="What is the catch?" class="TextBlock"> TODO </TextBlock>
+    </div>
+</template>
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import type { VaultTransaction } from 'auctions-core/src/types';
+import WhatIsMakerProtocol from '~/components/common/other/WhatIsMakerProtocol.vue';
+import TextBlock from '~/components/common/other/TextBlock.vue';
+
+export default Vue.extend({
+    components: {
+        TextBlock,
+        WhatIsMakerProtocol,
+    },
+    props: {
+        vaultTransactions: {
+            type: Array as PropType<VaultTransaction[]>,
+            default: () => [],
+        },
+        isVaultsLoading: {
+            type: Boolean,
+            default: false,
+        },
+        vaultsError: {
+            type: String,
+            default: null,
+        },
+        selectedVaultId: {
+            type: String,
+            default: null,
+        },
+        isExplanationsShown: {
+            type: Boolean,
+            default: true,
+        },
+        lastUpdated: {
+            type: Date,
+            default: null,
+        },
+    },
+});
+</script>
+
+<style scoped>
+.TextBlock {
+    @apply max-w-screen-sm self-center;
+}
+</style>
