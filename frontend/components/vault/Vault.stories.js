68d63e0758d925943996cbd2886ff0254db4e90a
Zoey <zoeykaiser8@gmail.com>
2022-09-20 11:07:03 +0200

    feat: New `Vault` component and stories (#455)

---
+++ frontend/components/vault/Vault.stories.js
@@ -0,0 +1,49 @@
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+import Vault from './Vault';
+import {
+    generateFakeVaultLiquidatedTransaction,
+    generateFakeVaultNotLiquidatedTransaction,
+} from '~/helpers/generateFakeVault';
+
+const fakeVaultNotLiquidatedTransaction = generateFakeVaultNotLiquidatedTransaction();
+const fakeVaultLiquidatedTransaction = generateFakeVaultLiquidatedTransaction();
+
+const common = {
+    components: {
+        Vault,
+    },
+    data() {
+        return {
+            vault: fakeVaultNotLiquidatedTransaction,
+            vaultId: fakeVaultLiquidatedTransaction.id.toString(),
+        };
+    },
+    methods: {
+        liquidate: action('liquidate'),
+    },
+};
+
+storiesOf('Vault/Vault', module)
+    .add('Default', () => ({
+        ...common,
+        template: `<Vault :vaultTransaction="vault" :vaultId='vaultId' @liquidate="liquidate" />`,
+    }))
+    .add('Finished', () => ({
+        ...common,
+        data() {
+            return {
+                vault: fakeVaultLiquidatedTransaction,
+                vaultId: fakeVaultLiquidatedTransaction.id.toString(),
+            };
+        },
+        template: `<Vault :vaultTransaction="vault" :vaultId='vaultId' @liquidate="liquidate" />`,
+    }))
+    .add('Fetching', () => ({
+        ...common,
+        template: `<Vault :are-vaults-fetching='true' :vaultId='vaultId' />`,
+    }))
+    .add('Not found', () => ({
+        ...common,
+        template: `<Vault :vaultId='vaultId' />`,
+    }));
