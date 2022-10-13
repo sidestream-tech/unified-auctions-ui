6170d5c8b93a8ffdada5050515c4e866a76f8631
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2022-10-13 10:16:24 +0200

    feat: CrvETH vault fetching support (#494)

--- core/src/vaults.ts
+++ core/src/vaults.ts
@@ -27,13 +27,49 @@ import { getOsmPrices } from './oracles';
 
 const CACHE_EXPIRY_MS = 60 * 1000;
 
-const _fetchVaultBase = async (network: string, id: number): Promise<VaultBase> => {
-    const contract = await getContract(network, 'CDP_MANAGER');
-    const address = await contract.urns(id);
+const _fetchVaultProxyAddress = async (network: string, proxyOwnerAddress: string) => {
+    const contract = await getContract(network, 'MCD_CROPPER');
+    const eventFilter = {
+        address: contract.address,
+        topics: [ethers.utils.id('NewProxy(address,address)'), ethers.utils.hexZeroPad(proxyOwnerAddress, 32), null],
+    } as ethers.EventFilter; // type defined so that it does not allow null as topic, although documentation allows it https://docs.ethers.io/v5/concepts/events/#events-solidity
+    const events = await contract.queryFilter(eventFilter);
+    if (events.length === 0) {
+        throw new Error(`Failed to extract the event 'NewProxy' for vault belonging to ${proxyOwnerAddress}`);
+    }
+    const vaultAddressHex32 = events[0].topics[2];
+    return ethers.utils.hexValue(vaultAddressHex32);
+};
+
+const fetchVaultProxyAddress = memoizee(_fetchVaultProxyAddress, {
+    promise: true,
+    length: 2,
+});
+
+const _fetchVaultAddress = async (network: string, id: number) => {
+    const cdpManager = await getContract(network, 'CDP_MANAGER');
+    const cdpRegistry = await getContract(network, 'CDP_REGISTRY');
+    const address = await cdpManager.urns(id);
     if (address === '0x0000000000000000000000000000000000000000') {
         throw new Error('Vault does not exist');
     }
-    const collateralTypeHex = await contract.ilks(id);
+    const owner = await cdpManager.owns(id);
+    if (owner === cdpRegistry.address) {
+        const proxyOwnerAddress = await cdpRegistry.owns(id);
+        return await fetchVaultProxyAddress(network, proxyOwnerAddress);
+    }
+    return address;
+};
+
+const fetchVaultAddress = memoizee(_fetchVaultAddress, {
+    promise: true,
+    length: 2,
+});
+
+const _fetchVaultBase = async (network: string, id: number): Promise<VaultBase> => {
+    const cdpManager = await getContract(network, 'CDP_MANAGER');
+    const address = await fetchVaultAddress(network, id);
+    const collateralTypeHex = await cdpManager.ilks(id);
     const collateralType = ethers.utils.parseBytes32String(collateralTypeHex);
     return {
         id,
