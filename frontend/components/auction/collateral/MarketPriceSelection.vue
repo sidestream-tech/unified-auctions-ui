3cbd0198b5527d843117f135b2bb48c8c0265ef8
Abdelrhman Farag <aomafarag@gmail.com>
2022-10-13 16:10:50 +0200

    Adjust component layout and finalize implementation

--- frontend/components/auction/collateral/MarketPriceSelection.vue
+++ frontend/components/auction/collateral/MarketPriceSelection.vue
@@ -13,27 +13,31 @@
             </div>
         </div>
         <CollapseTransition>
-            <div v-show="isExpanded" class="Content">
-                <div v-for="(value, key) in auctionTransaction.marketData" :key="key" class="flex justify-between">
-                    <div class="flex flex-row space-x-1">
-                        <div>{{ key }}</div>
-                        <div>
-                            (<span v-for="currency in value.route" :key="currency">{{ currency }} &#8594; </span>
-                            DAI)
-                        </div>
-                    </div>
-                    <div v-if="value.unitPrice && !value.unitPrice.isNaN()" class="flex flex-row space-x-1">
-                        <button type="button" @click="$emit('update:marketId', key)">
-                            <span v-if="marketId === key" class="opacity-50">Selected</span>
-                            <span v-else class="text-green-500">Select</span>
-                        </button>
-                        <div>
-                            <FormatCurrency :value="value.unitPrice" currency="DAI" /> per
-                            <span class="uppercase">{{ auctionTransaction.collateralSymbol }}</span>
-                        </div>
-                    </div>
-                    <div v-else class="justify-self-end opacity-50">Unknown</div>
-                </div>
+            <div v-show="isExpanded" class="Content overflow-x-auto">
+                <table class="table-auto">
+                    <tbody>
+                        <tr v-for="(value, key) in auctionTransaction.marketData" :key="key">
+                            <td class="pr-2 whitespace-nowrap">{{ key }}</td>
+                            <td class="pr-2 whitespace-nowrap">
+                                <span v-for="currency in value.route" :key="currency">{{ currency }} &#8594; </span>
+                                DAI
+                            </td>
+                            <td class="w-full text-right whitespace-nowrap">
+                                <div v-if="value.unitPrice && !value.unitPrice.isNaN()">
+                                    <button type="button" @click="$emit('update:marketId', key)">
+                                        <span v-if="marketId === key" class="opacity-50">Selected</span>
+                                        <span v-else class="text-green-500">Select</span>
+                                    </button>
+                                    <span class="pl-1">
+                                        <FormatCurrency :value="value.unitPrice" currency="DAI" /> per
+                                        <span class="uppercase">{{ auctionTransaction.collateralSymbol }}</span>
+                                    </span>
+                                </div>
+                                <div v-else class="opacity-50">Unknown</div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </CollapseTransition>
     </div>
