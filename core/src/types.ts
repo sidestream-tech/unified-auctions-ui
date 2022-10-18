3c0c2f41334ef283ffd6cb797c507a62fd895c4c
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-10-18 11:59:46 +0200

    Fixed build error

--- core/src/types.ts
+++ core/src/types.ts
@@ -63,7 +63,7 @@ export declare interface CollateralRow extends CollateralConfig, Partial<MakerPa
     marketUnitPrice?: BigNumber | string;
     tokenAddress?: string;
     tokenAddressError?: string;
-    autoRouteQuote?: number;
+    autoRouteQuote?: string;
     autoRouteExchanges?: string[];
     autoRouteError?: string;
 }
