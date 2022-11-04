78208a271c12a5e4f225d2932a908728e4fe8c28
Kirill Dogadin <kirillsc2@gmail.com>
2022-11-04 12:41:26 +0100

    unsupport

--- core/simulations/helpers/createVaultWithCollateral.ts
+++ core/simulations/helpers/createVaultWithCollateral.ts
@@ -39,6 +39,7 @@ import detectProxyTarget from '../../helpers/detectProxyTarget';
 
 const UNSUPPORTED_COLLATERAL_TYPES = [
     'RETH-A', // [temporary] this collateral is not yet deployed, tested via different flow
+    'TUSD-A', // [proxy] this collateral has a proxy-token contract and fallback solution does not work since JOIN contract does not have sufficient funds
 ];
 
 export const getLiquidatableCollateralTypes = () => {
