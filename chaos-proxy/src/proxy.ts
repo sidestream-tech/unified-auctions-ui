57abe8d0076407f54e0698dc943014512677fa26
Zoey Kaiser <zoeykaiser8@gmail.com>
2022-11-21 12:48:20 +0100

    Removed depricated chaos-proxy from project

--- chaos-proxy/src/proxy.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createProxyMiddleware } from 'http-proxy-middleware';
-
-export const startProxy = async function (targetUrl: string, port: number) {
-    const app = express();
-    app.use('/', createProxyMiddleware({ target: targetUrl, changeOrigin: true }));
-    app.listen(port);
-};
