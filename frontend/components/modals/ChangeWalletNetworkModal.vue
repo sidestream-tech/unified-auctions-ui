49d9b63fb40bce57262e84a235b4a634eb1521c1
Abdelrhman Farag <aomafarag@gmail.com>
2022-11-28 16:04:34 +0100

    Add missing import

--- frontend/components/modals/ChangeWalletNetworkModal.vue
+++ frontend/components/modals/ChangeWalletNetworkModal.vue
@@ -33,7 +33,8 @@
 <script lang="ts">
 import { Modal, Alert } from 'ant-design-vue';
 import Vue, { PropType } from 'vue';
-import BaseButton from '~/components/common/inputs/BaseButton';
+import { NetworkConfig } from 'auctions-core/src/types';
+import BaseButton from '~/components/common/inputs/BaseButton.vue';
 
 export default Vue.extend({
     name: 'ChangeWalletNetworkModal',
