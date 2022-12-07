ba8b1716f02c08e32b4314ed189856d84fe01b1b
Abdelrhman Farag <aomafarag@gmail.com>
2022-12-07 15:03:00 +0100

    Filter transactions on network

--- frontend/store/auctions.ts
+++ frontend/store/auctions.ts
@@ -54,11 +54,11 @@ const getInitialState = (): State => ({
 export const state = (): State => getInitialState();
 
 export const getters = {
-    listAuctions(state: State): AuctionTransaction[] {
-        return Object.values(state.auctionStorage);
-    },
-    listAuctionTransactions(state: State, getters: any, _rootState: any): AuctionTransaction[] {
-        const auctions = Object.values(state.auctionStorage);
+    // `rootGetters` is exposed to the `getters` module as the 4th parameter, right after `rootState`
+    // Hence the dummy _rootState parameter
+    listAuctionTransactions(state: State, getters: any, _rootState: any, rootGetters: any): AuctionTransaction[] {
+        const network = rootGetters['network/getMakerNetwork'];
+        const auctions = Object.values(state.auctionStorage).filter(auction => auction.network === network);
         return auctions.map(auction => {
             const isRestarting = getters.isAuctionRestarting(auction.id);
             return {
