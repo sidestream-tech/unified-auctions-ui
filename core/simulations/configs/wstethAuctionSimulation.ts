86b38510b54c319ad59aae884902ecdb3affbba5
Kirill Dogadin <kirillsc2@gmail.com>
2022-12-14 17:40:40 +0100

    as above :D

--- core/simulations/configs/wstethAuctionSimulation.ts
+++ core/simulations/configs/wstethAuctionSimulation.ts
@@ -1,15 +1,17 @@
 import { warpTime, resetNetworkAndSetupWallet } from '../../helpers/hardhat/network';
 import { Simulation } from '../types';
 
+const BLOCK_NUMBER = 14052140;
+
 const simulation: Simulation = {
-    title: 'Fork block with active WSTETH-A auction',
+    title: `Fork block ${BLOCK_NUMBER} with active WSTETH-A auction`,
     steps: [
         {
-            title: 'Reset blockchain fork',
+            title: `Reset blockchain fork to block ${BLOCK_NUMBER}`,
             // Few blocks before WSTETH-A is taken at 14052147,
             // https://etherscan.io/address/0x49a33a28c4c7d9576ab28898f4c9ac7e52ea457at
             entry: async () => {
-                await resetNetworkAndSetupWallet(14052140);
+                await resetNetworkAndSetupWallet(BLOCK_NUMBER);
             },
         },
         {
