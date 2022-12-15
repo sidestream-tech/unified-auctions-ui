3e571acfcb3e2261be8234ef697ccc328c5910fa
Kirill Dogadin <kirillsc2@gmail.com>
2022-12-15 10:52:07 +0100

    style: rename and lint

--- core/simulations/configs/wstethAuctionSimulation.ts
+++ core/simulations/configs/wstethAuctionSimulation.ts
@@ -2,6 +2,8 @@ import { warpTime, resetNetworkAndSetupWallet } from '../../helpers/hardhat/netw
 import promptToGetNumber from '../helpers/promptToGetNumber';
 import { Simulation } from '../types';
 
+// Few blocks before WSTETH-A is taken at 14052147,
+// https://etherscan.io/address/0x49a33a28c4c7d9576ab28898f4c9ac7e52ea457at
 const DEFAULT_BLOCK_NUMBER = 14052140;
 
 const simulation: Simulation = {
@@ -9,11 +11,9 @@ const simulation: Simulation = {
     steps: [
         {
             title: `Reset blockchain fork`,
-            // Few blocks before WSTETH-A is taken at 14052147,
-            // https://etherscan.io/address/0x49a33a28c4c7d9576ab28898f4c9ac7e52ea457at
             entry: async () => {
-                const number = await promptToGetNumber('Block number to fork from', DEFAULT_BLOCK_NUMBER);
-                await resetNetworkAndSetupWallet(number);
+                const selectedBlockNumber = await promptToGetNumber('Block number to fork from', DEFAULT_BLOCK_NUMBER);
+                await resetNetworkAndSetupWallet(selectedBlockNumber);
             },
         },
         {
