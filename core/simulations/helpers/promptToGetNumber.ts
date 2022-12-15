89a2bc84e676abde30c130e948413f15933164cf
Kirill Dogadin <kirillsc2@gmail.com>
2022-12-15 10:57:03 +0100

    style: rename and lint

--- core/simulations/helpers/promptToGetNumber.ts
+++ core/simulations/helpers/promptToGetNumber.ts
@@ -1,13 +1,13 @@
 import prompts from 'prompts';
 
-const promptToGetNumber = async (title: string, initial: number) => {
+const promptToGetNumber = async (title: string, initial: number, min = 1) => {
     const { number } = await prompts([
         {
             type: 'number',
             name: 'number',
             message: title,
             initial,
-            min: 1,
+            min,
         },
     ]);
     return number;
