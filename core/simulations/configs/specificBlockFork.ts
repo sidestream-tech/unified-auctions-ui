c56f11063320839fb8dbbbb42d95d55ad541674f
Kirill Dogadin <kirillsc2@gmail.com>
2022-12-15 16:24:43 +0100

    default to max block number everywhere

--- core/simulations/configs/specificBlockFork.ts
+++ core/simulations/configs/specificBlockFork.ts
@@ -3,10 +3,6 @@ import promptToGetNumber from '../helpers/promptToGetNumber';
 import { Simulation } from '../types';
 import hre from 'hardhat';
 
-// Few blocks before WSTETH-A is taken at 14052147,
-// https://etherscan.io/address/0x49a33a28c4c7d9576ab28898f4c9ac7e52ea457at
-const DEFAULT_BLOCK_NUMBER = 14052140;
-
 const simulation: Simulation = {
     title: `Fork at specific block`,
     steps: [
@@ -16,7 +12,7 @@ const simulation: Simulation = {
                 const latestBlock = await hre.ethers.provider.getBlockNumber();
                 const selectedBlockNumber = await promptToGetNumber(
                     'Block number to fork from',
-                    DEFAULT_BLOCK_NUMBER,
+                    latestBlock,
                     latestBlock
                 );
                 await resetNetworkAndSetupWallet(selectedBlockNumber);
