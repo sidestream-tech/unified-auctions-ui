2b19668eef8e5200c018015aa30f1556c990b12b
Kirill Dogadin <kirillsc2@gmail.com>
2022-12-15 10:01:09 +0100

    feat: add block number to another simulation

--- core/simulations/configs/wstethAuctionSimulation.ts
+++ core/simulations/configs/wstethAuctionSimulation.ts
@@ -1,17 +1,19 @@
 import { warpTime, resetNetworkAndSetupWallet } from '../../helpers/hardhat/network';
+import promptToGetNumber from '../helpers/promptToGetNumber';
 import { Simulation } from '../types';
 
-const BLOCK_NUMBER = 14052140;
+const DEFAULT_BLOCK_NUMBER = 14052140;
 
 const simulation: Simulation = {
-    title: `Fork block ${BLOCK_NUMBER} with active WSTETH-A auction`,
+    title: `Fork at specific block`,
     steps: [
         {
-            title: `Reset blockchain fork to block ${BLOCK_NUMBER}`,
+            title: `Reset blockchain fork`,
             // Few blocks before WSTETH-A is taken at 14052147,
             // https://etherscan.io/address/0x49a33a28c4c7d9576ab28898f4c9ac7e52ea457at
             entry: async () => {
-                await resetNetworkAndSetupWallet(BLOCK_NUMBER);
+                const number = await promptToGetNumber('Block number to fork from', DEFAULT_BLOCK_NUMBER);
+                await resetNetworkAndSetupWallet(number);
             },
         },
         {
