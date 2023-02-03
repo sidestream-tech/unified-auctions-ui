ba9c4e439e383216c3fae444fc0abacec61db474
yghazi <1640741+yghazi@users.noreply.github.com>
2023-02-03 23:41:57 +0500

    fix: Fix debt auctions bid units WAD->RAD (#572)

--- core/src/debt.ts
+++ core/src/debt.ts
@@ -1,9 +1,9 @@
 import type { DebtAuctionActive, DebtAuctionTransaction } from './types';
 import BigNumber from './bignumber';
 import getContract from './contracts';
-import { WAD_NUMBER_OF_DIGITS } from './constants/UNITS';
+import { RAD_NUMBER_OF_DIGITS, WAD_NUMBER_OF_DIGITS } from './constants/UNITS';
 import { Contract } from 'ethers';
-import { MKR_NUMBER_OF_DIGITS, WAD } from './constants/UNITS';
+import { MKR_NUMBER_OF_DIGITS, WAD, RAD } from './constants/UNITS';
 import memoizee from 'memoizee';
 import getNetworkDate from './date';
 import { CompensationAuctionBase, DebtAuction, Notifier, CompensationAuctionTransactionFees } from './types';
@@ -63,7 +63,7 @@ export const bidToDebtAuction = async function (
     const transactionParameters = [
         auctionIndex,
         acceptableReceivedAmount.shiftedBy(WAD_NUMBER_OF_DIGITS).toFixed(0),
-        auction.bidAmountDai.shiftedBy(WAD_NUMBER_OF_DIGITS).toFixed(0),
+        auction.bidAmountDai.shiftedBy(RAD_NUMBER_OF_DIGITS).toFixed(0),
     ];
     await executeTransaction(network, CONTRACT, 'dent', transactionParameters, { notifier });
 };
@@ -173,7 +173,7 @@ export const fetchDebtAuctionByIndex = async (network: string, auctionIndex: num
     return {
         ...baseAuctionInfo,
         earliestEndDate,
-        bidAmountDai: new BigNumber(auctionData.bid._hex).div(WAD),
+        bidAmountDai: new BigNumber(auctionData.bid._hex).div(RAD),
         receiveAmountMKR: new BigNumber(auctionData.lot._hex).div(WAD),
         receiverAddress: auctionData.guy,
         auctionEndDate,
