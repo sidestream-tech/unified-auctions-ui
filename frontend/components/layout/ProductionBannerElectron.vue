1f0fe65c4dc31032d4de52f8e4a72d831d93dc36
KirillDogadin-std <59374892+KirillDogadin-std@users.noreply.github.com>
2023-02-07 09:50:15 +0100

    wording

--- frontend/components/layout/ProductionBannerElectron.vue
+++ frontend/components/layout/ProductionBannerElectron.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="w-full py-2 text-center bg-yellow-400">
         <h1>
-            You can now run the Unified Auctions UI locally as an electron-packaged application. Please see its release
+            You can now run the Unified Auctions UI locally as a Desktop application. Please see its release
             <a class="underline" :href="url" target="_blank">{{ url }}</a> .
         </h1>
     </div>
