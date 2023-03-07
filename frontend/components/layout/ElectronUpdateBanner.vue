0a594d499cbef6c18d19f472f045f39fc61474fb
Abdelrhman Farag <aomafarag@gmail.com>
2023-03-07 16:44:13 +0100

    Add new case for production

--- frontend/components/layout/ElectronUpdateBanner.vue
+++ frontend/components/layout/ElectronUpdateBanner.vue
@@ -13,6 +13,13 @@
             >. Current version: v{{ version }}.
         </template>
     </Alert>
+    <Alert v-else-if="status === 'production'" type="info" show-icon banner closable class="text-center">
+        <template #message>
+            You can now run the Unified Auctions UI locally as a desktop application. Please see its release at
+            <a class="underline" :href="url" target="_blank">{{ url }}</a
+            >.
+        </template>
+    </Alert>
 </template>
 
 <script lang="ts">
