abbbc50ac5a0ed8d55c393b7e0b50528683927e2
KirillDogadin-std <kirillsc2@gmail.com>
2023-08-23 09:37:31 +0200

    prettier frontend

--- frontend/pages/surplus.vue
+++ frontend/pages/surplus.vue
@@ -1,5 +1,19 @@
 <template>
-    <span> This page has been deprecated </span>
+    <div class="flex flex-col w-full text-gray-300">
+        <span> This page has been deprecated </span>
+        <span>
+            Note: surplus auctions are deprecated since the introduction of
+            <a
+                class="text-blue-300"
+                href="https://forum.makerdao.com/t/introduction-of-smart-burn-engine-and-initial-parameters/21201"
+                >smart burn engine</a
+            >
+            via the
+            <a class="text-blue-300" href="https://vote.makerdao.com/polling/QmQmxEZp#poll-detail"
+                >spell 2023-06-23</a
+            ></span
+        >
+    </div>
     <!-- <SurplusContainer :network="network" /> -->
 </template>
 
