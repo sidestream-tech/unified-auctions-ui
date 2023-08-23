6a022ab7e00445e7c27693c8b5b766c148c1fc02
KirillDogadin-std <kirillsc2@gmail.com>
2023-08-23 12:08:43 +0200

    remove instead of comment

--- frontend/pages/surplus.vue
+++ frontend/pages/surplus.vue
@@ -16,22 +16,3 @@
     </div>
     <!-- <SurplusContainer :network="network" /> -->
 </template>
-
-<script lang="ts">
-/*
-import Vue from 'vue';
-import SurplusContainer from '~/containers/SurplusContainer.vue';
-
-export default Vue.extend({
-    components: {
-        SurplusContainer,
-    },
-    layout: process.env.DEMO_MODE ? 'demoMode' : 'default',
-    computed: {
-        network() {
-            return this.$store.getters['network/getPageNetwork'];
-        },
-    },
-});
-*/
-</script>
