b583f8e8b73be7790e5cb4fc20581fa557717c0a
DaeunYoon <daa3230@gmail.com>
2025-01-23 18:28:24 +0100

    feat: improve landing page style

---
+++ frontend/components/layout/HeroSection.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="HeroSection">
+        <LandingCard class="relative flex flex-col gap-y-8 py-24">
+            <h2 class="max-w-xl md:max-w-3xl text-5xl font-semibold text-balance whitespace-pre-line sm:text-7xl">
+                {{
+                    `The unified platform 
+                    to participate in the 
+                    Sky protocol auctions`
+                }}
+            </h2>
+        </LandingCard>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import LandingCard from '~/components/layout/LandingCard.vue';
+
+export default Vue.extend({
+    components: {
+        LandingCard,
+    },
+});
+</script>
+<style scoped>
+.HeroSection {
+    @apply w-full h-full;
+}
+.HeroBackground {
+    background-image: url('~/assets/images/background-image.jpg');
+}
+.dark .HeroBackground {
+    background-image: url('~/assets/images/background-image-dark.jpg');
+}
+</style>
--- frontend/components/unified/UnifiedAuctionsView.vue
+++ frontend/components/unified/UnifiedAuctionsView.vue
@@ -1,64 +1,68 @@
 <template>
-    <div class="HeightFix flex flex-col w-full h-full items-center">
-        <LandingBlock v-if="isExplanationsShown" class="LandingBlock">
-            <h1 class="text-gray-800 dark:text-gray-100">Unified Auctions</h1>
-        </LandingBlock>
-        <div class="flex flex-col w-full items-center space-y-4 md:space-y-8 mt-4 md:mt-8 mb-4 px-4">
-            <TextBlock v-if="isExplanationsShown" title="Different auction types" class="max-w-screen-sm">
-                There are three distinct auction types that help maintain
-                <Explain text="Maker Protocol’s">
-                    a <a href="https://changelog.makerdao.com/">set of smart contracts</a> running on the Ethereum
-                    blockchain with the purpose to keep the cryptoasset Dai approximately equal to USD
-                </Explain>
-                solvency. They are taking effect in different situations like liquidation of single debt positions, to
-                cap accrued system surplus or to cover system’s debt.
-            </TextBlock>
-            <AuctionTypeFilter :is-explanations-shown="isExplanationsShown" @selected="applyFilter" />
-            <div class="w-full max-w-screen-sm items-center">
-                <TextBlock
-                    v-if="isExplanationsShown"
-                    class="w-full mb-4"
-                    :title="filter ? 'Tools that support this auction type' : 'Existing auction tools'"
-                />
-                <transition-group name="Tool-List" class="space-y-4">
-                    <li
-                        v-for="tool in filteredToolList"
-                        :key="tool.title"
-                        class="Tool list-none w-full max-w-screen-sm"
-                    >
-                        <AuctionTool
-                            class="w-full"
-                            :title="tool.title"
-                            :is-explanations-shown="isExplanationsShown"
-                            :source-url="tool.links.source"
-                            :participate-url="tool.links.participate"
-                            :analytics-url="tool.links.analytics"
-                            :profile-url="tool.links.profile"
-                        >
-                            {{ tool.description }}
-                        </AuctionTool>
-                    </li>
-                </transition-group>
+    <div class="HeightFix w-full h-full space-y-20 pb-20">
+        <HeroSection class="HeroSection" />
+        <LandingCard>
+            <div class="w-full grid sm:grid-cols-2 xl:grid-cols-3 gap-4 auto-rows-max">
+                <AuctionCard
+                    v-for="tool in toolList"
+                    :key="tool.title"
+                    :title="tool.title"
+                    :content="tool.description"
+                >
+                    <div class="flex items-end gap-2">
+                        <LinkButton v-if="tool.links.source" type="secondary" :link="tool.links.source">
+                            Run your own bot
+                        </LinkButton>
+                        <LinkButton type="primary" :link="tool.links.participate">Participate via Ul</LinkButton>
+                    </div>
+                </AuctionCard>
             </div>
-        </div>
+        </LandingCard>
+        <LandingCard class="flex flex-col gap-y-6">
+            <h2 class="font-semibold text-xl dark:text-gray-200">Other existing auction tools</h2>
+            <div
+                class="
+                    divide-y divide-gray-200
+                    overflow-hidden
+                    rounded-lg
+                    bg-gray-200
+                    dark:bg-gray-600
+                    sm:grid sm:grid-cols-2 sm:gap-px sm:auto-rows-fr sm:divide-y-0
+                "
+            >
+                <div v-for="(tool, toolIndex) in legacyToolList" :key="tool.title" :class="getToolClass(toolIndex)">
+                    <div>
+                        <h3 class="text-base font-semibold text-gray-900 dark:text-gray-200 flex gap-2">
+                            <a :href="tool.links.source" class="focus:outline-none">
+                                <!-- Extend touch target to entire panel -->
+                                <span class="absolute inset-0" aria-hidden="true" />
+                                {{ tool.title }}
+                            </a>
+                            <ExternalLink class="w-5 h-5 ml-0.5 -mr-0.5 fill-current" />
+                        </h3>
+                        <p class="mt-2 text-sm text-gray-500 dark:text-gray-100">{{ tool.description }}</p>
+                    </div>
+                </div>
+            </div>
+        </LandingCard>
     </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import TextBlock from '~/components/common/other/TextBlock.vue';
-import AuctionTypeFilter from '~/components/unified/AuctionTypeFilter.vue';
-import Explain from '~/components/common/other/Explain.vue';
-import AuctionTool from '~/components/unified/AuctionTool.vue';
-import LandingBlock from '~/components/layout/LandingBlock.vue';
+import HeroSection from '~/components/layout/HeroSection.vue';
+import LandingCard from '~/components/layout/LandingCard.vue';
+import AuctionCard from '~/components/layout/AuctionCard.vue';
+import LinkButton from '~/components/common/inputs/LinkButton.vue';
+import ExternalLink from '~/assets/icons/external-link.svg';
 
 export default Vue.extend({
     components: {
-        TextBlock,
-        AuctionTypeFilter,
-        Explain,
-        AuctionTool,
-        LandingBlock,
+        HeroSection,
+        LandingCard,
+        AuctionCard,
+        LinkButton,
+        ExternalLink,
     },
     props: {
         isExplanationsShown: {
@@ -85,7 +89,6 @@ export default Vue.extend({
                     description:
                         'Web tool that supports participation in debt auctions by bidding on MKR with own Dai',
                     links: {
-                        source: 'https://github.com/sidestream-tech/unified-auctions-ui',
                         participate: '/debt',
                     },
                     filters: ['debt'],
@@ -95,68 +98,71 @@ export default Vue.extend({
                     description:
                         'Web tool that supports liquidation of vaults that are no longer collaterlised enough',
                     links: {
-                        source: 'https://github.com/sidestream-tech/unified-auctions-ui',
                         participate: '/vaults',
                     },
                     filters: ['collateral'],
                 },
+            ],
+            legacyToolList: [
                 {
-                    title: 'Liquidations platform',
+                    title: 'Collateral liquidation analytics',
+                    description: '',
+                    links: {
+                        source: 'https://info.sky.money/liquidations',
+                    },
+                    filters: ['collateral'],
+                },
+                {
+                    title: 'Legacy liquidation analytics',
+                    description: `
+                    Shows various statistics on current state of the system like current Dai supply,
+                    as well as collateral parameters and information on past liquidations`,
+                    links: {
+                        source: 'https://makerburn.com/#/liquidations',
+                    },
+                    filters: ['collateral', 'debt'],
+                },
+                {
+                    title: 'Source code of the exchange callees',
+                    description: '',
+                    links: {
+                        source: 'https://github.com/makerdao/exchange-callees',
+                    },
+                    filters: ['collateral', 'debt'],
+                },
+                {
+                    title: 'Source code of the legacy collateral auctions UI',
                     description: 'Web tool that support participation in collateral auctions by bidding with own Dai',
                     links: {
                         source: 'https://github.com/makerdao/liquidations-portal',
                     },
                     filters: ['collateral'],
                 },
-                {
-                    title: 'Collateral Auction Twitter Bot',
-                    description: `Tweets about collateral auctions and facilitates access to auction participation`,
-                    links: {
-                        profile: 'https://x.com/sidestream_labs',
-                    },
-                    filters: ['collateral'],
-                },
-                {
-                    title: 'MakerBurn',
-                    description: `
-                    Shows various statistics on current state of the system like current Dai supply,
-                    as well as collateral parameters and information on past liquidations`,
-                    links: {
-                        analytics: 'https://makerburn.com/',
-                    },
-                    filters: ['collateral', 'debt'],
-                },
             ],
         };
     },
-    computed: {
-        filteredToolList(): Array<Object> {
-            if (this.filter === '') {
-                return this.toolList;
-            }
-            return this.toolList.filter(tool => {
-                const filters = tool.filters as Array<Object>;
-                return filters.includes(this.filter);
-            });
-        },
-    },
     methods: {
-        applyFilter(selectedFilter: string) {
-            this.filter = selectedFilter;
+        getToolClass(toolIndex: number) {
+            const baseClass = 'ToolList group relative bg-white dark:bg-gray-800 p-6';
+            const roundedClasses = [
+                toolIndex === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
+                toolIndex === 1 ? 'sm:rounded-tr-lg' : '',
+                toolIndex === this.legacyToolList.length - 2 ? 'sm:rounded-bl-lg' : '',
+                toolIndex === this.legacyToolList.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '',
+            ];
+            return [baseClass, ...roundedClasses].join(' ');
         },
     },
 });
 </script>
 
 <style scoped>
-.PrimaryButton {
-    @apply px-4 py-1.5 border-2 rounded-md border-primary-light font-semibold text-primary;
+.HeroSection {
+    @apply w-full;
 }
 
-.LandingBlock {
-    @apply w-full;
-
-    min-height: 33vh;
+.PrimaryButton {
+    @apply px-4 py-1.5 border-2 rounded-md border-primary-light font-semibold text-primary;
 }
 
 .HeightFix {
@@ -178,5 +184,9 @@ export default Vue.extend({
 
 .Tool-List-leave-active {
     position: absolute;
+}
+
+.ToolList:hover {
+    @apply ring-2 ring-inset ring-primary-purple dark:ring-primary;
 }
 </style>
