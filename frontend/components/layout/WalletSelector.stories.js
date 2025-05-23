3168c5542daca88937e75f19f55c74b4c8545bda
Amirhossein Barari <amirhosseinbarari76@gmail.com>
2025-05-23 13:07:44 +0200

    remove storybook components and functions

--- frontend/components/layout/WalletSelector.stories.js
+++
@@ -1,7 +0,0 @@
-import { storiesOf } from '@storybook/vue';
-import WalletSelector from './WalletSelector.vue';
-
-storiesOf('Layout/WalletSelector', module).add('Default', () => ({
-    components: { WalletSelector },
-    template: `<WalletSelector/>`,
-}));
